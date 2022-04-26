c3b4a9ced080bd24553402190d008a8fc980a02a
John Zak <capt.zaks.gaming@gmail.com>
2022-04-25 22:15:06 -0400

    Trying to merge

--- Assets/_Scripts/Core/Game/GameManager.cs
+++ Assets/_Scripts/Core/Game/GameManager.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using Amoebius.Utility.Singleton;
 
-[DefaultExecutionOrder(0)]
-[RequireComponent(typeof(GameSetting))]
-public class GameManager : SingletonPersistent<GameManager>
+namespace StarWriter.Core
 {
-    private GameSetting gameSettings;
-
-    //TODO get AudioMaster dl
-    //set background music volume
-    // play music
-
-    // Start is called before the first frame update
-    void Start()
+    [DefaultExecutionOrder(0)]
+    [RequireComponent(typeof(GameSetting))]
+    public class GameManager : SingletonPersistent<GameManager>
     {
-        
-    }
+        private GameSetting gameSettings;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //TODO get AudioMaster dl
+        //set background music volume
+        // play music
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
+
