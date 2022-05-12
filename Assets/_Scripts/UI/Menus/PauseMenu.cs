f1b4f070243adf6f4328d79b4529e8d87caa679e
John Zak <capt.zaks.gaming@gmail.com>
2022-05-12 16:59:54 -0400

    Replacing missing Main Menu Design 1

--- Assets/_Scripts/UI/Menus/PauseMenu.cs
+++ Assets/_Scripts/UI/Menus/PauseMenu.cs
@@ -12,48 +12,68 @@ namespace StarWriter.UI
         GameManager gameManager;
 
         [SerializeField]
-        RectTransform musicToogleSprite;
+        Sprite musicToogleSprite;
         [SerializeField]
         Vector3 musicSpriteOffset;
+        bool isMuted = false;
 
         [SerializeField]
-        RectTransform gyroToogleSprite;
+        Sprite gyroToogleSprite;
         [SerializeField]
         Vector3 gyroSpriteOffset;
-        
+        bool gyroEnabled = true;
+
+        float xSpeed = 10f;
+
+        Vector2 musicOnPosition = new Vector2();
+        Vector2 musicOffPosition = new Vector2();
+
         // Start is called before the first frame update
         void Start()
         {
             gameManager = GameManager.Instance;
+            if(PlayerPrefs.GetInt("isMuted") == 0){ isMuted = false; }
+            if(PlayerPrefs.GetInt("isMuted") == 1) { isMuted = true; }
+            if (PlayerPrefs.GetInt("gyroEnabled") == 0) { gyroEnabled = false; }
+            if (PlayerPrefs.GetInt("gyroEnabled") == 1) { gyroEnabled = true; }
+
+            musicOnPosition = new Vector2(musicToogleSprite.rect.xMax, 0);
+            musicOffPosition = new Vector2(musicToogleSprite.rect.xMin, 0);
 
         }
 
-        public void OnMusicToggle(bool on)
+        private void Update()
+        {
+            if (isMuted)
+            {
+                //musicToogleSprite.rect.
+            }
+            else
+            {
+                //Spite moves to original position
+            }
+            if (gyroEnabled)
+            {
+                //Move Sprite 
+            }
+            else
+            {
+                //Spite moves to original position
+            }
+        }
+
+        public void OnMusicToggle(bool currentStatus)
         {
             StarWriter.Core.Audio.AudioManager audioManager = Core.Audio.AudioManager.Instance;
             audioManager.ToggleMute();
-            if (!on)
-            {
-                musicToogleSprite.Translate(musicToogleSprite.rect.position.x, musicToogleSprite.rect.position.y, 0);
-            }
-            if (on)
-            {
-                musicToogleSprite.Translate(musicSpriteOffset);
-            }
+            isMuted = !currentStatus;
             
         }
 
-        public void OnGyroToggle()
+        public void OnGyroToggle(bool currentStatus)
         {
             gameManager.OnClickGyroToggleButton();
-            if (PlayerPrefs.GetInt("gyroEnabled") == 0) 
-            {
-                gyroToogleSprite.Translate(gyroToogleSprite.rect.position.x, gyroToogleSprite.rect.position.y, 0);
-            }
-            if (PlayerPrefs.GetInt("gyroEnabled") == 1)
-            {
-                gyroToogleSprite.Translate(gyroSpriteOffset);
-            }
+            gyroEnabled = !currentStatus;
         }
 
         public void OnTutorialButtonPressed()
