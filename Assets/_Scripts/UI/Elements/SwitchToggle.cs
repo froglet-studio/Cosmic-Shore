fca003e08985a1649af5cc0e71e50b6a4211ae1b
John Zak <capt.zaks.gaming@gmail.com>
2022-05-18 13:02:13 -0400

    SwitchToggle

---
+++ Assets/_Scripts/UI/Elements/SwitchToggle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchToggle : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform handleRectTransform;
+
+    private Toggle toggle;
+
+    private Vector3 handlePosition = new Vector3(20,0,0);
+    
+    
+    void Awake()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(Toggled);
+    }
+
+    public void Toggled(bool status)
+    {
+        int sign;
+
+        sign = status ? -1 : 1;
+        handleRectTransform.localPosition += sign * handlePosition;
+    }
+
+    private void OnDestroy()
+    {
+        toggle.onValueChanged.RemoveListener(Toggled);
+    }
+}
