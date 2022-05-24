15c791b5eadddd5a0ebf4b90b06220e198b363f3
Garrett Milliron <gradies@gmail.com>
2022-05-23 20:35:54 -0400

    grace material collab kinda fixed the sign

--- Assets/_Scripts/UI/Elements/EndScorePositioner.cs
+++ Assets/_Scripts/UI/Elements/EndScorePositioner.cs
@@ -15,12 +15,14 @@ public class EndScorePositioner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         
+        transform.rotation = FollowTarget.transform.rotation;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = FollowTarget.transform.position + offset * (LookAtTarget.transform.position - FollowTarget.transform.position).normalized; 
+        transform.position = FollowTarget.transform.position + offset * ( LookAtTarget.transform.position - FollowTarget.transform.position).normalized;
+        transform.localRotation = Quaternion.Euler(0, FollowTarget.transform.rotation.eulerAngles.y - transform.localRotation.eulerAngles.y, 0) * transform.localRotation;
+
     }
 }
