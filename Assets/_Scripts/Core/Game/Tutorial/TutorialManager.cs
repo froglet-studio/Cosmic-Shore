89d61327b3b598a1aff5934ef5d715662f2d8eb3
Isaiah Merrill <isaiah.merrill@gmail.com>
2022-06-24 14:48:44 -0700

    Modify fuel bar

--- Assets/_Scripts/Core/Game/Tutorial/TutorialManager.cs
+++ Assets/_Scripts/Core/Game/Tutorial/TutorialManager.cs
@@ -132,7 +132,7 @@ namespace StarWriter.Core.Tutorial
                     dialogueText.text = stage.RetryLine.Text;
                     StartCoroutine(DelayFadeOfTextBox(stage.RetryLineDisplayTime));
                 }
-                stage.Retry();
+                BeginStage();
             }
             else
             {
