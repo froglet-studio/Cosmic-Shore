a93c124a159a9e928f0b09970b4d9024b76fbab0
John Zak <capt.zaks.gaming@gmail.com>
2022-06-29 15:15:06 -0400

    Audio System and Jukebox

--- Assets/_Scripts/Core/Audio/Song.cs
+++ Assets/_Scripts/Core/Audio/Song.cs
@@ -3,30 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class Song : MonoBehaviour
+namespace StarWriter.Core.Audio
 {
-    [SerializeField]
-    SO_Song song_SO;
-
-    string title;
-    string description;
-    string author;
-
-    AudioClip clip;
-
-    public string Title { get => title;  }
-    public string Description { get => description;  }
-    public string Author { get => author;  }
-    public AudioClip Clip { get => clip;  }
-
-    public void SetSongSO(SO_Song so)
+    public class Song : MonoBehaviour
     {
-        song_SO = so;
+        [SerializeField]
+        SO_Song song_SO;
 
-        clip = song_SO.Clip;
-        title = song_SO.Clip.name;
-        description = song_SO.Decription;
-        author = song_SO.Author;
+        string title;
+        string description;
+        string author;
+
+        AudioClip clip;
+
+        public string Title { get => title; }
+        public string Description { get => description; }
+        public string Author { get => author; }
+        public AudioClip Clip { get => clip; }
+
+        public void SetSongSO(SO_Song so)
+        {
+            song_SO = so;
+
+            clip = song_SO.Clip;
+            title = song_SO.Clip.name;
+            description = song_SO.Decription;
+            author = song_SO.Author;
+        }
+
     }
-
 }
+
