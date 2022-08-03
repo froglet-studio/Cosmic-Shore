883b81e117ae5bc109f120a8ae49cf8312f21218
Isaiah Merrill <isaiah.merrill@gmail.com>
2022-08-02 18:00:40 -0700

    Fix some bugs. Code cleanup.

--- Assets/_Scripts/Utility/Effects/BlockImpact.cs
+++ Assets/_Scripts/Utility/Effects/BlockImpact.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 
 public class BlockImpact : MonoBehaviour
 {
-    public IEnumerator ImpactCoroutine(Vector3 velocity, Material material,string ID)
+    public void HandleImpact(Vector3 velocity, string ID)
     {
-        var velocityScale = 100f;
-        var positionScale = 1;
+        StartCoroutine(ImpactCoroutine(velocity, ID));
+    }
+
+    private IEnumerator ImpactCoroutine(Vector3 velocity,string ID)
+    {
+        var velocityScale = .1f;
+        //var positionScale = 1;
         
         Vector3 distance = Vector3.zero;
+
+        Material material = GetComponent<MeshRenderer>().material;
 
         if (ID == "Player") { 
             material.SetFloat("_playerHit", 1); 
@@ -24,24 +31,15 @@ public class BlockImpact : MonoBehaviour
             }
         }
         
-        //while (timeStamp <= 100)
-        //{
-        //    yield return null;
-        //    timeStamp += .001f;
-        //    material.SetVector("_velocity", velocityScale*timeStamp*velocity);
-        //    material.SetFloat("_opacity", (1-timeStamp));
-        //    transform.position += (velocityScale * timeStamp * velocity);
-        //}
         while (distance.magnitude <= 1000)
         {
             yield return null;
-            //timeStamp += .001f;
             distance += velocityScale * Time.deltaTime * velocity;
             material.SetVector("_velocity", distance);
             material.SetFloat("_opacity", (1000 - distance.magnitude) / 1000);
-            transform.position += positionScale * distance;
+            //transform.position += positionScale * distance;
         }
-        Destroy(material);
+
         Destroy(transform.gameObject);
     }
 }
