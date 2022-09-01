040fe263805af6341b00849b4ade9f5899176d33
Isaiah Merrill <isaiah.merrill@gmail.com>
2022-09-01 12:08:35 -0700

    Control cursor stuff for maja

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controls : MonoBehaviour
 {
     [SerializeField] bool Left;
     [SerializeField] Vector2 offset;
-    
+    [SerializeField] Sprite InactiveImage;
+    [SerializeField] Sprite ActiveImage;
+
+    Image image;
     Vector2 initialPos;
 
     void Start()
     {
         initialPos = transform.position;
+        image = GetComponent<Image>();
+        image.sprite = InactiveImage;
     }
 
     void Update()
@@ -22,11 +28,13 @@ public class Controls : MonoBehaviour
         if (Input.touches.Length == 0)
         {
             transform.position = Vector2.Lerp(transform.position, initialPos, .2f);
+            image.sprite = InactiveImage;
         }
 
         //If there is only one touch, move closer control to the finger and start following
         else if (Input.touches.Length == 1)
         {
+            image.sprite = ActiveImage;
             if (Input.touches[0].position.x <= Screen.currentResolution.width / 2)
             {
                 if (Left)
@@ -57,7 +65,7 @@ public class Controls : MonoBehaviour
         // If there are two touches assign one controller per touch and follow
         else if (Input.touches.Length == 2)
         {
-
+            image.sprite = ActiveImage;
             if (Input.touches[0].position.x <= Input.touches[1].position.x)
             {
                 leftTouch = Input.touches[0].position;
