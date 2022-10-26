b7f1f17a78f845de989c5b1899df37eb3414974b
Garrett Milliron <gradies@gmail.com>
2022-10-26 14:30:18 -0400

    skimmer tweaks

--- Assets/Skimmer.cs
+++ Assets/Skimmer.cs
@@ -23,7 +23,7 @@ public class Skimmer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = Vector3.Lerp(direction, Vector3.zero, .01f);
+        direction = Vector3.Lerp(direction, Vector3.zero, .02f);
         material.SetVector("_direction", direction);
     }
 
@@ -32,7 +32,7 @@ public class Skimmer : MonoBehaviour
         Debug.Log("skim collider: " + other.name);
         OnSkim?.Invoke(ship.GetComponent<Player>().PlayerUUID, fuelAmount);
 
-        direction = Vector3.Lerp(direction, (transform.worldToLocalMatrix * (other.transform.position - transform.position)).normalized * 19,.1f);
+        direction = Vector3.Lerp(direction, (transform.worldToLocalMatrix * (other.transform.position - transform.position)).normalized*100f,.05f);
         material.SetVector("_direction", direction);
     }
 }
