153a344980a156880aa0be32eb3361a31aa22c4f
Garrett Milliron <gradies@gmail.com>
2022-11-29 22:37:55 -0500

    like a smooth gunner

--- Assets/_Scripts/_Core/AI/AIGunner.cs
+++ Assets/_Scripts/_Core/AI/AIGunner.cs
@@ -6,41 +6,58 @@ public class AIGunner : MonoBehaviour
 {
 
     TrailSpawner trailSpawner;
-    float blockIndex;
+    int nextBlockIndex;
+    int previousblockIndex;
     float gunnerSpeed;
+    float lerpAmount;
     bool direction;
     int gap = 3;
+
 
     // Start is called before the first frame update
     void Start()
     {
         trailSpawner = transform.parent.GetComponent<TrailSpawner>();
-        gunnerSpeed = .1f;
-        blockIndex = 0;
+        gunnerSpeed = 5f;
+        nextBlockIndex = 1;
         direction = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        lerpAmount += gunnerSpeed * Time.deltaTime;
         if (direction)
         {
-            blockIndex += gunnerSpeed;
+            if (lerpAmount > 1)
+            {
+                previousblockIndex = nextBlockIndex;
+                nextBlockIndex++;
+                lerpAmount -= 1f;
+            }
         }
-        if (blockIndex > trailSpawner.trailList.Count - gap - gunnerSpeed)
+        if (nextBlockIndex > trailSpawner.trailList.Count - gap)
         {
             direction = false;
         }
         if (!direction)
         {
-            blockIndex -= gunnerSpeed;
+            if (lerpAmount > 1)
+            {
+                previousblockIndex = nextBlockIndex;
+                nextBlockIndex--;
+                lerpAmount -= 1f;
+            }
         }
-        if (blockIndex < gap)
+        if (nextBlockIndex < gap)
         {
             direction = true;
         }
-        Debug.Log($"block index: {blockIndex}");
-        transform.position = trailSpawner.trailList[(int)blockIndex].transform.position;
-        if (trailSpawner.trailList[(int)blockIndex].destroyed) trailSpawner.trailList[(int)blockIndex].restore();
+        Debug.Log($"block index: {nextBlockIndex}");
+        
+        transform.position = Vector3.Lerp(trailSpawner.trailList[previousblockIndex].transform.position,
+                                          trailSpawner.trailList[nextBlockIndex].transform.position,
+                                          lerpAmount);
+        if (trailSpawner.trailList[(int)nextBlockIndex].destroyed) trailSpawner.trailList[(int)nextBlockIndex].restore();
     }
 }
