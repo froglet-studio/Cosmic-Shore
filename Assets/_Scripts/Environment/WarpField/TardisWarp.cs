8ac98ce06659fb9ef84fa5b18175b3e26804f2cf
Garrett Milliron <gradies@gmail.com>
2022-12-09 18:34:26 -0500

    tardis tweak and dolphin start

--- Assets/_Scripts/Environment/WarpField/TardisWarp.cs
+++ Assets/_Scripts/Environment/WarpField/TardisWarp.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [CreateAssetMenu(fileName = "TardisWarpData", menuName = "ScriptableObjects/TardisWarp", order = 2)]
 [System.Serializable] public class TardisWarp : WarpFieldSO
@@ -13,16 +15,24 @@ using UnityEngine;
         fieldMax = 500f;
     }
 
+    float minRadius = 10;
+
     override public Vector3 HybridVector(Transform node)
     {
-        return new Vector3(3*Mathf.Atan(node.position.x / fieldMax),
-                           3*Mathf.Atan(node.position.y/ fieldMax),
-                           3*Mathf.Atan(node.position.z/ fieldMax));
+        return new Vector3(sigmoid(node.position.x),
+                           sigmoid(node.position.y),
+                           sigmoid(node.position.z));
     }
 
     float Gaussian(float input, float sigma, float distance)
     {
         float twoSigmaSquared = 2 * Mathf.Pow(sigma, 2);
         return Mathf.Exp(-Mathf.Pow(input - distance, 2) / twoSigmaSquared);
+    }
+
+    float sigmoid(float position)
+    {
+        if (position > 0) return 3 * Mathf.Atan(position - minRadius) / fieldMax;
+        else return 3 * Mathf.Atan(position + minRadius) / fieldMax;
     }
 }
