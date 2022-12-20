603e8c2a0c7a60d6fb7fba6a3ac8a46c28a9c3e9
Maja <maja@froglet.studio>
2022-12-20 16:54:19 -0500

    I tried to make the shark's skimmer smash things
    I feel like I'm close but feel free to revert if not.

--- Assets/_Scripts/_Core/Control/Skimmer.cs
+++ Assets/_Scripts/_Core/Control/Skimmer.cs
@@ -1,3 +1,6 @@
+using StarWriter.Core.Input;
+using StarWriter.Core;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Skimmer : MonoBehaviour
@@ -5,12 +8,29 @@ public class Skimmer : MonoBehaviour
     [SerializeField] Ship ship;
     [SerializeField] public Player Player;
     [SerializeField] float fuelAmount;
+    [SerializeField] List<SkimmerImpactEffects> skimmerImpactEffects;
     public bool thief;
 
 
     // TODO: move this away from using an event
     public delegate void Skim(string uuid, float amount);
     public static event Skim OnSkim;
+
+    //Maja added this to try and enable shark skimmer smashing
+    public void PerformSkimmerImpactEffects(TrailBlockProperties trailBlockProperties)
+    {
+        foreach (SkimmerImpactEffects effect in skimmerImpactEffects)
+        {
+            switch (effect)
+            {
+                case SkimmerImpactEffects.PlayHaptics:
+                    HapticController.PlayBlockCollisionHaptics();
+                    break;
+                case SkimmerImpactEffects.DeactivateTrailBlock:
+                    break;
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
