e8d36f290809c29da4ebd4cae6e3b8529f1ef093
Garrett Milliron <gradies@gmail.com>
2023-02-25 14:05:11 -0500

    dartboard prefab with tweaks, and a red block

--- Assets/_Scripts/Environment/MiniGameObjects/DartBoard.cs
+++ Assets/_Scripts/Environment/MiniGameObjects/DartBoard.cs
@@ -9,14 +9,15 @@ public class DartBoard : MonoBehaviour
 {
     [SerializeField] TrailBlock trailBlock;
     float blockCount = 6; // TODO: make int
-    int ringCount = 6;
-    float ringThickness = 30f;
-    Vector3 blockScale = new Vector3(20f, 10f, 5f);
+    int ringCount = 25;
+    float ringThickness = 5f;
+    float gap = 6;
+    //Vector3 blockScale = new Vector3(3f, 2f, 1f);
 
 
     //dartboard position
     float dartBoardRadius = 300;
-    public float difficultyAngle = 20;
+    public float difficultyAngle = 5;
     //Vector3 dartBoardPosition;
 
     //[SerializeField] Material blockMaterial;
@@ -26,7 +27,7 @@ public class DartBoard : MonoBehaviour
     {
         transform.position = Quaternion.Euler(
             Random.Range(0,360), 
-            Random.Range(0, difficultyAngle), 
+            Random.Range(Mathf.Max(difficultyAngle - 30, 5) , difficultyAngle), 
             0) * (dartBoardRadius * Vector3.forward);
 
         transform.LookAt(Vector3.zero);
@@ -35,26 +36,29 @@ public class DartBoard : MonoBehaviour
 
     void CreateRings()
     {
-        for (int ring = 0; ring < ringCount; ring++)
+        for (int ring = 1; ring <= ringCount; ring++)
         {
             trails.Add(new Trail());
             for (int block = 0; block < blockCount*ring; block++)
             {
-                CreateRingBlock(block, ring % 2 * .5f, ring / 2f + 1f, 0, 0, trails[ring], ring);
+                CreateRingBlock(block, ring % 2 * .5f, 0, 0, trails[ring-1], ring);
             }
         }
     }
 
-    void CreateRingBlock(int i, float phase, float scale, float tilt, float sweep, Trail trail, int ring)
+    void CreateRingBlock(int i, float phase, float tilt, float sweep, Trail trail, int ring)
     {
         var position = transform.position +
-                             scale * ringThickness * Mathf.Cos(((i + phase) / (blockCount * ring)) * 2 * Mathf.PI) * transform.right +
-                             scale * ringThickness * Mathf.Sin(((i + phase) / (blockCount * ring)) * 2 * Mathf.PI) * transform.up +
+                             ring * ringThickness * Mathf.Cos(((i + phase) / (blockCount * ring)) * 2 * Mathf.PI) * transform.right +
+                             ring * ringThickness * Mathf.Sin(((i + phase) / (blockCount * ring)) * 2 * Mathf.PI) * transform.up +
                              sweep * ringThickness * transform.forward;
-        CreateBlock(position, position + tilt * ringThickness * transform.forward, "::AOE::" + Time.time + "::" + i, trail);
+        CreateBlock(position, position + tilt * ringThickness * transform.forward, "::AOE::" + Time.time + "::" + i, trail,
+            new Vector3(((Mathf.PI / 3f) * ringThickness) - (gap / (6 * ring)), 
+                        (ringCount - ring) * ringThickness/4f,
+                         ringThickness - (gap/5f)));
     }
 
-    void CreateBlock(Vector3 position, Vector3 lookPosition, string ownerId, Trail trail)
+    void CreateBlock(Vector3 position, Vector3 lookPosition, string ownerId, Trail trail, Vector3 scale)
     {
         var Block = Instantiate(trailBlock);
         //Block.Team = Team;
@@ -63,7 +67,7 @@ public class DartBoard : MonoBehaviour
         Block.transform.SetPositionAndRotation(position, Quaternion.LookRotation(lookPosition - transform.position, transform.forward));
         //Block.GetComponent<MeshRenderer>().material = blockMaterial;
         Block.ID = Block.ownerId + ownerId;
-        Block.InnerDimensions = blockScale;
+        Block.InnerDimensions = scale;
         //Block.transform.parent = TrailSpawner.TrailContainer.transform;
         Block.Trail = trail;
         trail.Add(Block);
