a97f7fa28d76aa400f70299b59fc252bdffe6f75
Maja <maja@froglet.studio>
2023-03-08 14:35:44 -0500

    Okay Last round of fixes for now

--- Assets/_Scripts/UI/Menus/MainMenu.cs
+++ Assets/_Scripts/UI/Menus/MainMenu.cs
@@ -12,6 +12,7 @@ namespace StarWriter.Core.UI
         public GameObject Main_Menu_Panel;
         public GameObject Minigames_Panel;
         public GameObject Records_Panel;
+        public GameObject Ship_Select;
 
         GameManager gameManager;
 
@@ -50,6 +51,7 @@ namespace StarWriter.Core.UI
             Main_Menu_Panel.SetActive(false);
             Minigames_Panel.SetActive(false);
             Records_Panel.SetActive(false);
+            Ship_Select.SetActive(false);
         }
         public void OnClickRecords()
         {
@@ -58,6 +60,7 @@ namespace StarWriter.Core.UI
             Main_Menu_Panel.SetActive(false);
             Minigames_Panel.SetActive(false);
             Records_Panel.SetActive(true);
+            Ship_Select.SetActive(false);
         }
         public void OnClickMinigames()
         {
@@ -66,6 +69,7 @@ namespace StarWriter.Core.UI
             Main_Menu_Panel.SetActive(false);
             Hangar_Panel.SetActive(false);
             Records_Panel.SetActive(false);
+            Ship_Select.SetActive(false);
         }   
         public void OnClickOptionsMenuButton()
         {
@@ -74,6 +78,7 @@ namespace StarWriter.Core.UI
             Hangar_Panel.SetActive(false);
             Minigames_Panel.SetActive(false);
             Records_Panel.SetActive(false);
+            Ship_Select.SetActive(false);
         }
         public void OnClickHome()
         {
@@ -82,6 +87,7 @@ namespace StarWriter.Core.UI
             Hangar_Panel.SetActive(false);
             Minigames_Panel.SetActive(false);
             Records_Panel.SetActive(false);
+            Ship_Select.SetActive(false);
         }
 
     }
