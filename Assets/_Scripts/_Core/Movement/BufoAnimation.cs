dc8f524d281a2e0e8e408b0972a44d430d5ac6b5
Garrett Milliron <gradies@gmail.com>
2023-05-01 19:20:17 -0400

    Bufo animations

--- Assets/_Scripts/_Core/Movement/BufoAnimation.cs
+++ Assets/_Scripts/_Core/Movement/BufoAnimation.cs
@@ -6,28 +6,89 @@ class BufoAnimation : ShipAnimation
 {
 
     [SerializeField] Transform Fusilage;
+    [SerializeField] Transform Turret;
 
+    [SerializeField] Transform ThrusterTopRight;
+    [SerializeField] Transform ThrusterRight;
+    [SerializeField] Transform ThrusterBottomRight;
+    [SerializeField] Transform ThrusterBottomLeft;
+    [SerializeField] Transform ThrusterLeft;
+    [SerializeField] Transform ThrusterTopLeft;
 
-    [SerializeField] float animationScaler = 25f;
+    public List<Transform> Transforms; //TODO: use this to populate the ship geometries on ship.cs 
+
+    static float animationScaler = 32f;
+    float exaggeratedAnimationScaler = 1.4f * animationScaler;
     [SerializeField] float yawAnimationScaler = 80f;
     [SerializeField] float lerpAmount = 2f;
     [SerializeField] float smallLerpAmount = .7f;
+
+    private void Start()
+    {
+        Transforms.Add(Fusilage);
+        Transforms.Add(Turret);
+        Transforms.Add(ThrusterTopRight);
+        Transforms.Add(ThrusterRight);
+        Transforms.Add(ThrusterBottomRight);
+        Transforms.Add(ThrusterBottomLeft);
+        Transforms.Add(ThrusterLeft);
+        Transforms.Add(ThrusterTopLeft);
+    }
 
     public override void PerformShipAnimations(float pitch, float yaw, float roll, float throttle)
     {
         // Ship animations TODO: figure out how to leverage a single definition for pitch, etc. that captures the gyro in the animations.
 
         AnimatePart(Fusilage,
-                    -pitch * animationScaler,
-                    yaw * animationScaler,
-                    roll * animationScaler);
+            pitch * animationScaler,
+            yaw * animationScaler,
+            roll * animationScaler);
+
+        AnimatePart(Turret,
+            pitch * exaggeratedAnimationScaler*.7f,
+            yaw * exaggeratedAnimationScaler,
+            roll * exaggeratedAnimationScaler);
+
+        AnimatePart(ThrusterTopRight,
+            pitch * exaggeratedAnimationScaler,
+            yaw * exaggeratedAnimationScaler,
+            roll * exaggeratedAnimationScaler);
+
+        AnimatePart(ThrusterRight,
+            pitch * exaggeratedAnimationScaler,
+            yaw * exaggeratedAnimationScaler,
+            roll * exaggeratedAnimationScaler);
+
+        AnimatePart(ThrusterBottomRight,
+            pitch * exaggeratedAnimationScaler,
+            yaw * exaggeratedAnimationScaler,
+            roll * exaggeratedAnimationScaler);
+
+        AnimatePart(ThrusterBottomLeft,
+            pitch * exaggeratedAnimationScaler,
+            yaw * exaggeratedAnimationScaler,
+            roll * exaggeratedAnimationScaler);
+
+        AnimatePart(ThrusterLeft,
+            pitch * exaggeratedAnimationScaler,
+            yaw * exaggeratedAnimationScaler,
+            roll * exaggeratedAnimationScaler);
+
+        AnimatePart(ThrusterTopLeft,
+            pitch * exaggeratedAnimationScaler,
+            yaw * exaggeratedAnimationScaler,
+            roll * exaggeratedAnimationScaler);
     }
 
     public override void Idle()
     {
-
-        Fusilage.localRotation = Quaternion.Lerp(Fusilage.localRotation, Quaternion.identity, smallLerpAmount * Time.deltaTime);
+        foreach (Transform transform in Transforms)
+        {
+            resetAnimation(transform);
+        }
     }
+
+    void resetAnimation(Transform part) { part.localRotation = Quaternion.Lerp(part.localRotation, Quaternion.identity, smallLerpAmount * Time.deltaTime); }
 
     void AnimatePart(Transform part, float partPitch, float partYaw, float partRoll)
     {
@@ -40,12 +101,4 @@ class BufoAnimation : ShipAnimation
                                     lerpAmount * Time.deltaTime);
     }
 
-    float Brake(float throttle)
-    {
-        var brakeThreshold = .65f;
-        float newThrottle;
-        if (throttle < brakeThreshold) newThrottle = throttle - brakeThreshold;
-        else newThrottle = 0;
-        return newThrottle;
-    }
 }
