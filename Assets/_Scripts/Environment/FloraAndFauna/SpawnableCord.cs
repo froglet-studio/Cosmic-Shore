3287eb4b5fef4c6718285318561e7a1d7dfefcd7
Garrett Milliron <gradies@gmail.com>
2023-08-24 15:21:12 -0400

    updates to the way devastating works (doesn't destroy blocks)and more progress on seaweed

--- Assets/_Scripts/Environment/FloraAndFauna/SpawnableCord.cs
+++ Assets/_Scripts/Environment/FloraAndFauna/SpawnableCord.cs
@@ -1,7 +1,6 @@
 using StarWriter.Core;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.ParticleSystem;
 
 public class SpawnableCord : SpawnableAbstractBase
 {
@@ -14,15 +13,19 @@ public class SpawnableCord : SpawnableAbstractBase
     [SerializeField] float length = 150;
     static int ObjectsSpawned = 0;
 
-    List<Bone> bones = new List<Bone>();
+    List<List<Bone>> ListofBoneLists = new List<List<Bone>>();
+    List <LineRenderer> lineRenderers= new List<LineRenderer>();
+
+    List<Bone> bones;
     GameObject container;
     LineRenderer lineRenderer = new();
-
+    int verticesPerBone = 1;
 
 
 
     public override GameObject Spawn()
     {
+        bones = new List<Bone>();
         container = new GameObject();
         container.name = "Cord" + ObjectsSpawned++;
 
@@ -30,7 +33,7 @@ public class SpawnableCord : SpawnableAbstractBase
         Vector3[] vertices = new Vector3[verticesCount];
 
         // Create the bone structure
-        int bonesCount = verticesCount / 10;
+        int bonesCount = verticesCount / verticesPerBone;
         
         Bone previousBone = null;
         for (int i = 0; i < bonesCount; i++)
@@ -56,8 +59,8 @@ public class SpawnableCord : SpawnableAbstractBase
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.startColor = Color.green;
         lineRenderer.endColor = Color.blue;
-        lineRenderer.startWidth = 0.05f;
-        lineRenderer.endWidth = 0.4f;
+        lineRenderer.startWidth = 0.5f;
+        lineRenderer.endWidth = 2f;
         lineRenderer.positionCount = vertices.Length;
         lineRenderer.transform.SetParent(container.transform, false);
         lineRenderer.useWorldSpace = false;
@@ -65,15 +68,17 @@ public class SpawnableCord : SpawnableAbstractBase
         for (int vertex = 0; vertex < verticesCount; vertex++)
         {
             // Determine the influencing bone based on vertex position
-            Bone influencingBone = bones[vertex / 10];
+            Bone influencingBone = bones[vertex / verticesPerBone];
 
             // Get position from the bone and adjust using the original logic if needed
-            Vector3 bonePosition = influencingBone.Transform.position;
+            
 
             var t = (float)vertex / verticesCount * Mathf.PI * 12;
             var x = (Mathf.Sin(t) * xc1) + (Mathf.Sin(t * xc2 + xc3) * xc4);
             var y = (Mathf.Cos(t) * yc1) + (Mathf.Cos(t * yc2 + yc3) * yc4);
             var position = new Vector3(x, y, t * length / (Mathf.PI * 12));
+
+            influencingBone.Transform.position = position;
 
             lineRenderer.SetPosition(vertex, position);
 
@@ -94,25 +99,37 @@ public class SpawnableCord : SpawnableAbstractBase
             bone.Animate(Time.deltaTime);
         }
 
+        ListofBoneLists.Add(bones);
+        lineRenderers.Add(lineRenderer);
+
         trails.Add(trail);
         return container;
+        
     }
 
     private void Update()
     {
-        // Animate bones to create the sway effect
-        foreach (var bone in bones)
+ 
+        for (var i = 0; i < ListofBoneLists.Count; i++)
         {
-            bone.Animate(Time.deltaTime);
-        }
+            foreach (var bone in ListofBoneLists[i])
+            {
+                bone.Animate(Time.deltaTime);
+            }
 
-        // Update the vertex positions of the LineRenderer based on the bones' positions
-        for (int vertex = 0; vertex < verticesCount; vertex++)
-        {
-            Bone influencingBone = bones[vertex / 10];
-            Vector3 position = influencingBone.Transform.position;
-            lineRenderer.SetPosition(vertex, position);
+            // Update the vertex positions of the LineRenderer based on the bones' positions
+            for (int vertex = 0; vertex < verticesCount; vertex++)
+            {
+                Bone influencingBone = ListofBoneLists[i][vertex / verticesPerBone];
+                Vector3 position = influencingBone.Transform.position;
+                lineRenderers[i].SetPosition(vertex, position);
+                lineRenderers[i].widthCurve = lineRenderer.widthCurve;
+                if (vertex % (verticesCount / blockCount) == 0)
+                {
+                    var block = vertex / (verticesCount / blockCount);
+                    trails[i].GetBlock(block).transform.localPosition = position;
+                }
+            }
         }
     }
-
 }
