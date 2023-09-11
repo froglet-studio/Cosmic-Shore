fb7afb31912b1a8ee839d604a8664ac532f26945
Isaiah Merrill <isaiah.merrill@gmail.com>
2023-09-11 08:38:19 -0700

    Add account manager and a catalog manager

---
+++ Assets/_Scripts/AccountManager.cs
@@ -0,0 +1,174 @@
+using StarWriter.Utility.Singleton;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using Newtonsoft.Json;
+
+public class AccountManager : SingletonPersistent<AccountManager>
+{
+
+    public delegate void OnLoginSuccessEvent();
+    public static event OnLoginSuccessEvent onLoginSuccess;
+
+    public static string PlayerId;
+    public static string PlayFabId;
+    public static PlayFabAuthenticationContext AuthenticationContext;
+    public static string EntityType;
+    static List<string> Adjectives;
+    static List<string> Nouns;
+
+    [SerializeField] TMPro.TMP_Text DisplayName;
+ 
+    public static string PlayerDisplayName = "";
+
+    public void SetPlayerDisplayName(string playerName)
+    {
+        PlayFab.PlayFabClientAPI.UpdateUserTitleDisplayName(
+            new PlayFab.ClientModels.UpdateUserTitleDisplayNameRequest()
+            {
+                DisplayName = playerName
+            },
+            (UpdateUserTitleDisplayNameResult result) =>
+            {
+                PlayerDisplayName = result.DisplayName;
+                DisplayName.text = result.DisplayName;
+            },
+            (PlayFabError error) =>
+            {
+                Debug.Log(error.GenerateErrorReport());
+            }
+        );
+    }
+
+    public void GenerateRandomDisplayName()
+    {
+        int adjectiveIndex = Random.Range(0, Adjectives.Count);
+        string adjective = Adjectives[adjectiveIndex];
+        int nounIndex = Random.Range(0, Nouns.Count);
+        string noun = Nouns[nounIndex];
+
+        string name = adjective + noun;
+        Debug.Log($"Display Name: {name}");
+        SetPlayerDisplayName(name);
+    }
+
+    public string GetPlayerDisplayName()
+    {
+        return PlayerDisplayName;
+    }
+
+    public void SetRandomDisplayName()
+    {
+        int adjectiveIndex = Random.Range(0, Adjectives.Count);
+        string adjective = Adjectives[adjectiveIndex];
+        int nounIndex = Random.Range(0, Nouns.Count);
+        string noun = Nouns[nounIndex];
+
+        string name = adjective + noun;
+        Debug.Log($"Display Name: {name}");
+
+
+    }
+
+    void Start()
+    {
+        StartCoroutine(DoTheThingsCoroutine());
+    }
+
+    IEnumerator DoTheThingsCoroutine()
+    {
+        Login();
+
+        yield return new WaitForSeconds(1);
+
+        LoadPlayerProfile();
+    }
+
+    void LoadPlayerProfile()
+    {
+        PlayFab.PlayFabClientAPI.GetPlayerProfile(
+            new PlayFab.ClientModels.GetPlayerProfileRequest()
+            {
+                PlayFabId = PlayFabId,
+            },
+            result =>
+            {
+                Debug.Log($"Load Player Profile: {result.PlayerProfile.DisplayName}");
+            },
+            error =>
+            {
+                Debug.Log(error.GenerateErrorReport());
+            }
+        );
+    }
+
+    void LoadTitleData()
+    {
+        PlayFabClientAPI.GetTitleData(
+            new GetTitleDataRequest()
+            {
+                AuthenticationContext = AuthenticationContext,
+            },
+            result =>
+            {
+                foreach (var item in result.Data.Keys)
+                {
+                    Debug.Log(item);
+                }
+                if (result.Data == null || !result.Data.ContainsKey("DefaultDisplayNameAdjectives"))
+                    Debug.Log("No DefaultDisplayNameAdjectives");
+                else
+                {
+                    Debug.Log("DefaultDisplayNameAdjectives: " + result.Data["DefaultDisplayNameAdjectives"]);
+                    Debug.Log("DefaultDisplayNameNouns: " + result.Data["DefaultDisplayNameNouns"]);
+                    //string jsonString = "[\"String1\", \"String2\", \"String3\"]";
+                    //var deserialized = JsonConvert.DeserializeObject(result.Data);
+                    Adjectives = new(JsonConvert.DeserializeObject<string[]>(result.Data["DefaultDisplayNameAdjectives"]));
+
+                    Nouns = new(JsonConvert.DeserializeObject<string[]>(result.Data["DefaultDisplayNameNouns"]));
+                }
+            },
+            error =>
+            {
+                Debug.Log("Got error getting titleData:");
+                Debug.Log(error.GenerateErrorReport());
+            }
+        );
+    }
+
+    void Login()
+    {
+        if (AuthenticationContext != null)
+            return;
+
+#if (UNITY_ANDROID || UNITY_EDITOR)
+        PlayFabClientAPI.LoginWithAndroidDeviceID(
+            new LoginWithAndroidDeviceIDRequest()
+            {
+                CreateAccount = true,
+                AndroidDeviceId = SystemInfo.deviceUniqueIdentifier
+            }, 
+            result =>
+            {
+                
+                PlayerId = result.EntityToken.Entity.Id;
+                PlayFabId = result.PlayFabId;
+                AuthenticationContext = result.AuthenticationContext;
+                EntityType = result.EntityToken.Entity.Type;
+                Debug.Log($"Logged in: {result.PlayFabId}");
+                Debug.Log($"Entity Type: {EntityType}");
+                Debug.Log($"PlayerId: {PlayerId}");
+
+                onLoginSuccess?.Invoke();
+                LoadTitleData();
+            }, 
+            error =>
+            {
+                Debug.LogError(error.GenerateErrorReport());
+            }
+        );
+#endif
+    }
+}
---
+++ Assets/_Scripts/CatalogManager.cs
@@ -0,0 +1,203 @@
+using PlayFab;
+using PlayFab.EconomyModels;
+using StarWriter.Utility.Singleton;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatalogManager : SingletonPersistent<CatalogManager>
+{
+    // Items list, configurable via inspector
+    static List<CatalogItem> CatalogItems;
+    static List<CatalogItem> InventoryItems;
+
+
+    // Bootstrap the whole thing
+    public void Start()
+    {
+        AccountManager.onLoginSuccess += LoadCatalog;
+    }
+
+    void LoadCatalog()
+    {
+        PlayFabEconomyAPI.SearchItems(
+            new()
+            {
+                AuthenticationContext = AccountManager.AuthenticationContext,
+                Filter = "ContentType eq 'Vessel' and tags/any(t: t eq 'Rhino')"
+            },
+            response =>
+            {
+                CatalogItems = response.Items;
+                Debug.Log(CatalogItems);
+                foreach (var item in CatalogItems)
+                {
+                    Debug.Log("   Title: " + item.Title);
+                    Debug.Log("   Content Type: " + item.ContentType);
+                    foreach (var description in item.Description.Values)
+                    {
+                        Debug.Log("   Description: " + description);
+                    }
+                    Debug.Log("   DefaultStackId: " + item.DefaultStackId);
+                    Debug.Log("   Id: " + item.Id);
+                }
+            },
+            error => 
+            { 
+                Debug.Log(error.ErrorDetails); 
+            }
+        );
+    }
+
+    // TODO: Add title data key of starting catalog item ids
+
+    void GrantStartingInventory()
+    {
+        for (int i = 0; i < CatalogItems.Count; i++)
+        {
+            PlayFabEconomyAPI.AddInventoryItems(
+                new AddInventoryItemsRequest()
+                {
+                    AuthenticationContext = AccountManager.AuthenticationContext,
+                    Amount = 100,
+                    Item = new InventoryItemReference
+                    {
+                        Id = CatalogItems[i].Id
+                    },
+                    Entity = new EntityKey
+                    {
+                        Id = AccountManager.PlayerId,
+                        Type = AccountManager.EntityType
+                    }
+                },
+                response =>
+                {
+                    Debug.Log("OnAddInventoryItemSuccess");
+                    foreach (var transactionId in response.TransactionIds)
+                    {
+                        Debug.Log($"transaction id: {transactionId}");
+                    }
+                },
+                error =>
+                {
+                    Debug.LogError(error.GenerateErrorReport());
+                }
+            );
+        }
+    }
+
+    void LoadInventory()
+    {
+        PlayFabEconomyAPI.GetInventoryItems(
+            new GetInventoryItemsRequest
+            {
+                AuthenticationContext = AccountManager.AuthenticationContext,
+            },
+            response =>
+            {
+                Debug.Log("GetInventoryItemsResponse: " + response.Items);
+
+                List<string> itemIds = new();
+                foreach (var item in response.Items)
+                {
+                    itemIds.Add(item.Id);
+                }
+
+                PlayFabEconomyAPI.GetItems(
+                    new GetItemsRequest()
+                    {
+                        Ids = itemIds
+                    },
+                    response =>
+                    {
+                        InventoryItems = response.Items;
+
+                        foreach (var item in response.Items)
+                        {
+                            Debug.Log("   Id: " + item.Id);
+                            foreach (var key in item.Title.Keys)
+                            {
+                                Debug.Log("   Title Key: " + key);
+                                Debug.Log("   Title: " + item.Title[key]);
+                            }
+                            Debug.Log("   Type: " + item.Type);
+                            Debug.Log("   Image Count: " + item.Images.Count);
+                            Debug.Log("   Content Type: " + item.ContentType);
+                        }
+                    },
+                    error =>
+                    {
+                        Debug.LogError(error.GenerateErrorReport());
+                    });
+            },
+            error =>
+            {
+                Debug.LogError(error.GenerateErrorReport());
+            }
+        );
+    }
+
+    public void GetInventoryItem(string inventoryItemId)
+    {
+        PlayFabEconomyAPI.GetItem(
+            new GetItemRequest()
+            {
+                Id = inventoryItemId
+            },
+            (GetItemResponse response) =>
+            {
+                Debug.Log("   Id: " + response.Item.Id);
+                foreach (var key in response.Item.Title.Keys)
+                {
+                    Debug.Log("   Title Key: " + key);
+                    Debug.Log("   Title: " + response.Item.Title[key]);
+                }
+                Debug.Log("   Type: " + response.Item.Type);
+                Debug.Log("   Image Count: " + response.Item.Images.Count);
+                Debug.Log("   Content Type: " + response.Item.ContentType);
+            },
+            (PlayFabError error) =>
+            {
+                Debug.LogError(error.GenerateErrorReport());
+            }
+        );
+    }
+
+    public void BuyTomahawk()
+    {
+        string tomahawkId = "1fc47256-60e1-4976-b66e-71feb8e56372";
+        string shardId = "017bbd32-c4f7-486b-ab86-f899fda1f4ca";
+        PlayFabEconomyAPI.PurchaseInventoryItems(
+            new()
+            {
+                AuthenticationContext = AccountManager.AuthenticationContext,
+
+                Amount = 1,
+                Item = new() 
+                { 
+                    Id = tomahawkId
+                },
+                Entity = new()
+                {
+                    Id = AccountManager.PlayerId,
+                    Type = AccountManager.EntityType
+                },
+                PriceAmounts = new List<PurchasePriceAmount>
+                {
+                    new PurchasePriceAmount() 
+                    { 
+                        ItemId = shardId, 
+                        Amount = 10 
+                    }
+                },
+            },
+            response =>
+            {
+                Debug.Log("Successfully purchased Tomahawk");
+            },
+            error =>
+            {
+                Debug.LogError(error.GenerateErrorReport());
+            }
+        );
+    }
+}
