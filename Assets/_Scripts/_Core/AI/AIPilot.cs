1c5b47e14bc6413abf6a17efc7e6cd5d54ddb9c1
Garrett Milliron <gradies@gmail.com>
2023-09-16 00:10:04 -0400

    Apped Pip with scaling added overload for vector3 to lerping coroutine

--- Assets/_Scripts/_Core/AI/AIPilot.cs
+++ Assets/_Scripts/_Core/AI/AIPilot.cs
@@ -73,6 +73,7 @@ namespace StarWriter.Core.IO
 
         RaycastHit hit;
         float maxDistance = 50f;
+        float maxDistanceSquared;
 
         // TODO: rename to 'TargetTransform'
         public Transform CrystalTransform;
@@ -102,29 +103,7 @@ namespace StarWriter.Core.IO
         }
         #endregion
 
-        void Start()
-        {
-            ship = GetComponent<Ship>();
-            if (AutoPilotEnabled) 
-            { 
-                ship.InputController.AutoPilotEnabled = true;
-                ship.ShipStatus.AutoPilotEnabled = true;
-            }
-            aggressiveness = defaultAggressiveness;
-            throttle = defaultThrottle;
-            shipData = GetComponent<ShipStatus>();
-
-            CornerBehaviors = new Dictionary<Corner, AvoidanceBehavior>() {
-                { Corner.TopRight, new AvoidanceBehavior (raycastWidth, raycastHeight, Clockwise, Vector3.zero ) },
-                { Corner.BottomRight, new AvoidanceBehavior (raycastWidth, -raycastHeight, CounterClockwise, Vector3.zero ) },
-                { Corner.BottomLeft, new AvoidanceBehavior (-raycastWidth, -raycastHeight, Clockwise, Vector3.zero ) },
-                { Corner.TopLeft, new AvoidanceBehavior (-raycastWidth, raycastHeight, CounterClockwise, Vector3.zero ) }
-            };
-
-            var activeNode = NodeControlManager.Instance.GetNodeByPosition(transform.position);
-            activeNode.RegisterForUpdates(this);
-            if (useAbility) StartCoroutine(UseAbilityCoroutine(ability));
-        }
+        
 
         public void NodeContentUpdated()
         {
@@ -156,65 +135,82 @@ namespace StarWriter.Core.IO
             CrystalTransform = closestItem == null ? activeNode.transform : closestItem.transform;
         }
 
+        void Start()
+        {
+            maxDistanceSquared = maxDistance * maxDistance;
+            ship = GetComponent<Ship>();
+            if (AutoPilotEnabled)
+            {
+                ship.InputController.AutoPilotEnabled = true;
+                ship.ShipStatus.AutoPilotEnabled = true;
+            }
+            aggressiveness = defaultAggressiveness;
+            throttle = defaultThrottle;
+            shipData = GetComponent<ShipStatus>();
+
+            CornerBehaviors = new Dictionary<Corner, AvoidanceBehavior>() {
+                { Corner.TopRight, new AvoidanceBehavior (raycastWidth, raycastHeight, Clockwise, Vector3.zero ) },
+                { Corner.BottomRight, new AvoidanceBehavior (raycastWidth, -raycastHeight, CounterClockwise, Vector3.zero ) },
+                { Corner.BottomLeft, new AvoidanceBehavior (-raycastWidth, -raycastHeight, Clockwise, Vector3.zero ) },
+                { Corner.TopLeft, new AvoidanceBehavior (-raycastWidth, raycastHeight, CounterClockwise, Vector3.zero ) }
+            };
+
+            var activeNode = NodeControlManager.Instance.GetNodeByPosition(transform.position);
+            activeNode.RegisterForUpdates(this);
+
+        }
+
         void Update()
         {
             if (AutoPilotEnabled)
             {
+                if (useAbility) StartCoroutine(UseAbilityCoroutine(ability));
                 Vector3 distance = CrystalTransform.position - transform.position;
 
                 Vector3 desiredDirection = distance.normalized;
-                LookingAtCrystal = Vector3.Dot(desiredDirection, shipData.Course) >= .91;
+                LookingAtCrystal = Vector3.Dot(desiredDirection, shipData.Course) >= .93;
+                if (LookingAtCrystal && drift)
+                {
+                    shipData.Drifting = true;
+                    desiredDirection *= -1;
+                }
+                else shipData.Drifting = false;
 
                 if (distance.magnitude < float.Epsilon) // Avoid division by zero
                     return;
 
-                Vector3 combinedLocalCrossProduct = Vector3.zero;
+                Vector3 combinedAdjustment = Vector3.zero;
+                Dictionary<Corner, Vector3> obstacleDirections = new Dictionary<Corner, Vector3>();
+
 
                 foreach (Corner corner in Enum.GetValues(typeof(Corner)))
                 {
                     var behavior = CornerBehaviors[corner];
                     Vector3 laserHitDirection = ShootLaser(behavior.width * transform.right + behavior.height * transform.up);
-                    float clampedLerp = Mathf.Clamp(avoidance / laserHitDirection.magnitude, 0, 0.9f);
-                    Vector3 adjustedDirection = TurnAway(desiredDirection, laserHitDirection, (-transform.up + (behavior.spin * transform.right)), clampedLerp); // TODO: avoidance is wip
-                    Vector3 crossProduct = Vector3.Cross(transform.forward, adjustedDirection);
-                    Vector3 localCrossProduct = transform.InverseTransformDirection(crossProduct);
-                    combinedLocalCrossProduct += localCrossProduct;
+                    float scaledAvoidance = Mathf.Clamp(avoidance / laserHitDirection.sqrMagnitude, -1, 1);
+                    Vector3 adjustment = CalculatePitchAndYawAdjustments(desiredDirection, laserHitDirection, scaledAvoidance);
+                    combinedAdjustment += adjustment;
+
+                    obstacleDirections[corner] = laserHitDirection;
                 }
 
-                float angle = Mathf.Asin(Mathf.Clamp(combinedLocalCrossProduct.magnitude * aggressiveness / Mathf.Min(distance.magnitude, maxDistance * 10), -1f, 1f)) * Mathf.Rad2Deg;
+                combinedAdjustment.x = -Mathf.Clamp(combinedAdjustment.x*4  / Mathf.Min(distance.magnitude, maxDistance), -1, 1); // Pitch
+                combinedAdjustment.y = Mathf.Clamp(combinedAdjustment.y*4  / Mathf.Min(distance.magnitude, maxDistance), -1, 1); // Yaw
 
-                if (LookingAtCrystal && drift) shipData.Drifting = true; 
-                else shipData.Drifting = false;
+                // Roll adjustment
+                float rollAdjustment = CalculateRollAdjustment(obstacleDirections);
+                //combinedAdjustment.z = Mathf.Clamp(rollAdjustment, -1, 1); // Roll
 
-                YSum = (LookingAtCrystal && drift) ? .1f : Mathf.Clamp(angle * combinedLocalCrossProduct.x, -1, 1);
-                XSum = (LookingAtCrystal && drift) ? .3f : Mathf.Clamp(angle * combinedLocalCrossProduct.y, -1, 1);
-                YDiff = (LookingAtCrystal && drift) ? .05f :Mathf.Clamp(angle * combinedLocalCrossProduct.z, -1, 1);
+                XSum = combinedAdjustment.x;
+                YSum = combinedAdjustment.y;
+                YDiff = Mathf.Clamp(CalculateRollAdjustment(obstacleDirections), -1, 1);
+                XDiff = (LookingAtCrystal && ram) ? 1 : Mathf.Clamp(throttle, 0, 1);
+
                 ///get better
                 aggressiveness += aggressivenessIncrease * Time.deltaTime;
                 throttle += throttleIncrease * Time.deltaTime;
-
-                XDiff = (LookingAtCrystal && ram) ? 1 : Mathf.Clamp(throttle, 0, 1);
-                
             }
 
-        }
-
-        Vector3 TurnAway(Vector3 originalDirection, Vector3 obstacleDirection, Vector3 rotationDirection, float avoidanceFactor)
-        {
-            if (obstacleDirection == Vector3.zero) // No obstacle detected
-                return originalDirection;
-
-            float dotProduct = Vector3.Dot(originalDirection, obstacleDirection.normalized);
-            if (dotProduct > 0) // Obstacle is in the direction we want to move
-                return originalDirection + (rotationDirection * avoidanceFactor);
-
-            return originalDirection;
-        }
-
-        float SigmoidResponse(float input)
-        {
-            float output = 2 * (1 / (1 + Mathf.Exp(-0.1f * input)) - 0.5f);
-            return output;
         }
 
 
@@ -223,14 +219,51 @@ namespace StarWriter.Core.IO
             if (Physics.Raycast(transform.position + position, transform.forward, out hit, maxDistance))
             {
                 Debug.DrawLine(transform.position + position, hit.point, Color.red);
-                return hit.point - transform.position;
+                return hit.point - (transform.position + position);
             }
             else
             {
-                Debug.DrawLine(transform.position + position, (transform.position + position) + transform.forward * maxDistance, Color.green);
-                return transform.forward - transform.position;
+                Debug.DrawLine(transform.position + position, transform.position + position + transform.forward * maxDistance, Color.green);
+                return transform.forward * maxDistance - (transform.position + position);
             }
         }
+
+        Vector3 CalculatePitchAndYawAdjustments(Vector3 originalDirection, Vector3 obstacleDirection, float avoidanceFactor)
+        {
+            Vector3 adjustment = Vector3.zero;
+
+            // Primary objective adjustments
+            adjustment.y = Vector3.Dot(originalDirection, transform.right);   // Yaw
+            adjustment.x = Vector3.Dot(originalDirection, transform.up);    // Pitch
+
+            //if (obstacleDirection != Vector3.zero) // If obstacle detected
+            //{
+            //    float dotProduct = Vector3.Dot(originalDirection, obstacleDirection.normalized);
+            //    if (dotProduct > 0.2) // Obstacle is in the desired direction or slightly off-course
+            //    {
+            //        // Calculate obstacle-based yaw and pitch adjustments
+            //        adjustment.y += Vector3.Dot(obstacleDirection, transform.right) * avoidanceFactor;   // Yaw
+            //        adjustment.x += Vector3.Dot(obstacleDirection, transform.up) * avoidanceFactor;     // Pitch
+            //    }
+            //}
+
+            return adjustment;
+        }
+
+
+        float CalculateRollAdjustment(Dictionary<Corner, Vector3> obstacleDirections)
+        {
+            float rollAdjustment = 0f;
+
+            // Example logic: If top right and bottom left corners detect obstacles, induce a roll.
+            if (obstacleDirections[Corner.TopRight].magnitude > 0 && obstacleDirections[Corner.BottomLeft].magnitude > 0)
+                rollAdjustment -= 1; // Roll left
+            if (obstacleDirections[Corner.TopLeft].magnitude > 0 && obstacleDirections[Corner.BottomRight].magnitude > 0)
+                rollAdjustment += 1; // Roll right
+
+            return rollAdjustment;
+        }
+
 
         IEnumerator UseAbilityCoroutine(ShipActionAbstractBase action) 
         {
@@ -239,6 +272,14 @@ namespace StarWriter.Core.IO
                 yield return new WaitForSeconds(abilityCooldown); // wait first to give the resource system time to load
                 action.StartAction();
             }
+            action.StopAction();
+        }
+
+
+        float SigmoidResponse(float input)
+        {
+            float output = 2 * (1 / (1 + Mathf.Exp(-0.1f * input)) - 0.5f);
+            return output;
         }
 
     }
