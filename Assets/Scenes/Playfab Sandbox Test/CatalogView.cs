f9b905cff947c60d79afdc0e051eaebb0461b463
Echo Yin <echo_ybl@hotmail.com>
2023-09-24 14:22:53 -0400

    Catalog Test view

--- Assets/Scenes/Playfab Sandbox Test/CatalogView.cs
+++ Assets/Scenes/Playfab Sandbox Test/CatalogView.cs
@@ -8,7 +8,17 @@ public class CatalogView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        purchaseVesselButton.onClick.AddListener(CatalogManager.Instance.PurchaseItemTest);
+        purchaseVesselButton.onClick.AddListener(PurchaseItemTest);
     }
     
+    /// <summary>
+    /// Purchase Item Test
+    /// Buy a test vessel using shards, the amount of shards should be the exact price tag on the test vessel shards
+    /// </summary>
+    private void PurchaseItemTest()
+    {
+        string vesselId = "aaf7670c-96a2-46d8-9489-0d971c6dc742";
+        string shardId = "88be4041-cc48-4231-8595-d440b371d015";
+        CatalogManager.Instance.PurchaseItem(vesselId, shardId, 1, 5);
+    }
 }
