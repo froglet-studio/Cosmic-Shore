479c7aee1f9095288ade279e9636a451d8898dcc
Isaiah Merrill <isaiah.merrill@gmail.com>
2023-10-03 13:45:40 -0700

    Introduce ElementalFloats && ElementalShipComponents. Cleanup in ResourceSystem

---
+++ Assets/_Scripts/_Core/Ship/ElementalFloat.cs
@@ -0,0 +1,44 @@
+﻿using StarWriter.Core;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class ElementalFloat
+{
+    [SerializeField] public float Value;
+    [SerializeField] float Min;
+    [SerializeField] float Max;
+    [SerializeField] Element element;
+    Ship ship;
+    string name;
+
+    public ElementalFloat(float value)
+    {
+        Value = value;
+    }
+
+    public string Name
+    {
+        set { name = value; }
+    }
+
+    public Ship Ship
+    {
+        set
+        {
+            ship = value;
+
+            if (element != Element.None)
+            {
+                ship.NotifyElementalFloatBinding(name, element);
+                ship.ResourceSystem.OnElementLevelChange += ScaleValueWithLevel;
+            }
+        }
+    }
+
+    void ScaleValueWithLevel(Element element, int level)
+    {
+        if (element == this.element)
+            Value = Mathf.Lerp(Min, Max, level / 10f);
+    }
+}
