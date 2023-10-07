d1126668d1f60b041ce5a2d69eb72fede590b93c
Echo Yin <echo_ybl@hotmail.com>
2023-10-07 16:59:41 -0400

    Added some juicy stuff and file relocation

---
+++ Assets/_Scripts/_Core/Firebase Analytics/Models/FirebaseUserModel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace _Scripts._Core.Firebase_Models
+{
+    public class FirebaseUserModel
+    {
+        // Associate the user id with unity device unique identifier
+        public string UniqueId { get; set; }
+        // TODO: more user properties coming up
+    }
+}
