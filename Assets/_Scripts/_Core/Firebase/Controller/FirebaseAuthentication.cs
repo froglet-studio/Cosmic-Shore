0a81be22a5c45adf8b7846ea76308a3d59113074
Echo Yin <echo_ybl@hotmail.com>
2023-10-13 20:44:00 -0400

    Added Firebase Crashlytics SDK

--- Assets/_Scripts/_Core/Firebase/Controller/FirebaseAuthentication.cs
+++ Assets/_Scripts/_Core/Firebase/Controller/FirebaseAuthentication.cs
@@ -5,7 +5,10 @@ namespace _Scripts._Core.Firebase.Controller
 {
     public class FirebaseAuthentication : SingletonPersistent<FirebaseAuthentication>
     {
+        // User authentication
         private static FirebaseAuth _userAuthentication;
+        
+        // Developer authentication, recommended use: in UNITY_EDITOR directive
         private static FirebaseAuth _devAuthentication;
     }
 }
