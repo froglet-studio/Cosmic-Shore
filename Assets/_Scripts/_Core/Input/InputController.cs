074e05dbd26676e8f983dbaa9eb55bdee4ed5fd5
Echo Yin <echo_ybl@hotmail.com>
2023-11-02 17:22:37 -0400

    Bugfix: input controller gyro actions fix

--- Assets/_Scripts/_Core/Input/InputController.cs
+++ Assets/_Scripts/_Core/Input/InputController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Gamepad = UnityEngine.InputSystem.Gamepad;
+using Gyroscope = UnityEngine.Gyroscope;
+using TouchPhase = UnityEngine.TouchPhase;
 
 namespace StarWriter.Core.IO
 {
@@ -63,8 +66,10 @@ namespace StarWriter.Core.IO
             LeftJoystickValue = LeftClampedPosition = LeftJoystickHome = new Vector2(JoystickRadius, JoystickRadius);
             RightJoystickValue = RightClampedPosition = RightJoystickHome = new Vector2(Screen.currentResolution.width - JoystickRadius, JoystickRadius);
 
+            // Enable Input gyro and then give the value to the local properties, otherwise it will only give null value
+            Input.gyro.enabled = true;
             gyro = Input.gyro;
-            gyro.enabled = true;
+            
             StartCoroutine(GyroInitializationCoroutine());
             invertYEnabled = GameSetting.Instance.InvertYEnabled;       
         }
@@ -223,7 +228,12 @@ namespace StarWriter.Core.IO
                             if (!rightStickEffectsStarted)
                             {
                                 rightStickEffectsStarted = true;
-                                ship.PerformShipControllerActions(InputEvents.RightStickAction);
+                                // if (ship == null)
+                                // {
+                                //     Debug.LogWarningFormat("{0} - {1} - {2}", nameof(InputController), nameof(ReceiveInput), "ship object is null.");
+                                // }
+                                if(ship != null)
+                                    ship.PerformShipControllerActions(InputEvents.RightStickAction);
                             }
                             RightJoystickValue = position;
                             rightTouchIndex = 0;
@@ -315,6 +325,24 @@ namespace StarWriter.Core.IO
 
         public Quaternion GetGyroRotation()
         {
+            // if (Input.gyro == null)
+            // {
+            //     Debug.LogWarningFormat("{0} - {1} - {2}", 
+            //         nameof(InputController), nameof(GetGyroRotation), "input gyro is null");
+            // }
+            // if(gyro.attitude == null)
+            //     Debug.LogWarningFormat("{0} - {1} - {2}: {3}", 
+            //         nameof(InputController), nameof(GetGyroRotation), nameof(inverseInitialRotation), inverseInitialRotation.ToString());
+            // if(inverseInitialRotation == null)
+            //     Debug.LogWarningFormat("{0} - {1} - {2}: {3}", 
+            //         nameof(InputController), nameof(GetGyroRotation), nameof(inverseInitialRotation), inverseInitialRotation.ToString());
+            // if(GyroQuaternionToUnityQuaternion(gyro.attitude) == null)
+            //     Debug.LogWarningFormat("{0} - {1} - {2}: {3}", 
+            //         nameof(InputController), nameof(GetGyroRotation), "GyroQuaternionToUnityQuaternion(gyro.attitude)", GyroQuaternionToUnityQuaternion(gyro.attitude).ToString());
+            // if(derivedCorrection == null)
+            //     Debug.LogWarningFormat("{0} - {1} - {2}: {3}", 
+            //         nameof(InputController), nameof(GetGyroRotation), nameof(derivedCorrection), derivedCorrection.ToString());
+            
             return inverseInitialRotation * GyroQuaternionToUnityQuaternion(gyro.attitude) * derivedCorrection;
         }
 
