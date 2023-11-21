01e653ef8d5ba4f22bf291a60c398088a14fbb1a
Echo Yin <echo_ybl@hotmail.com>
2023-11-20 19:59:38 -0500

    Console logging for NodeItems

--- Assets/_Scripts/Game/Environment/MiniGameObjects/NodeItem.cs
+++ Assets/_Scripts/Game/Environment/MiniGameObjects/NodeItem.cs
@@ -25,16 +25,28 @@ public class NodeItem : MonoBehaviour
 
     public void AddSelfToNode()
     {
+        if (NodeControlManager.Instance == null)
+        {
+            Debug.LogErrorFormat("{0} - {1} - {2} is not instantiated yet. Would be good to attach it somewhere?", nameof(NodeItem), nameof(AddSelfToNode), nameof(NodeControlManager));
+        }
         NodeControlManager.Instance.AddItem(this);
     }
 
     public void RemoveSelfFromNode()
     {
+        if (NodeControlManager.Instance == null)
+        {
+            Debug.LogErrorFormat("{0} - {1} - {2} is not instantiated yet. Would be good to attach it somewhere?", nameof(NodeItem), nameof(RemoveSelfFromNode), nameof(NodeControlManager));
+        }
         NodeControlManager.Instance.RemoveItem(this);
     }
 
     public void UpdateSelfWithNode()
     {
+        if (NodeControlManager.Instance == null)
+        {
+            Debug.LogErrorFormat("{0} - {1} - {2} is not instantiated yet. Would be good to attach it somewhere?", nameof(NodeItem), nameof(UpdateSelfWithNode), nameof(NodeControlManager));
+        }
         NodeControlManager.Instance.UpdateItem(this);
     }
 }
