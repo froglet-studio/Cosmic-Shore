d2a55d2ff3e1d81ab2c305f9f71dbece61bc2f06
KidIgarus <isaiah.merrill@gmail.com>
2023-12-04 17:01:13 -0800

    Work in progress - New Hangar Menu

--- Assets/_Scripts/App/Systems/UserAction/UserActionTrigger.cs
+++ Assets/_Scripts/App/Systems/UserAction/UserActionTrigger.cs
@@ -4,8 +4,11 @@ namespace CosmicShore.App.Systems.UserActions
 {
     public class UserActionTrigger : MonoBehaviour
     {
+        [Tooltip("Required")]
         [SerializeField] UserActionType type;
+        [Tooltip("Optional - Must be set if using 'TriggerAction'")]
         [SerializeField] int Value;
+        [Tooltip("Optional - Must be set if using 'TriggerAction'")]
         [SerializeField] string Label;
 
         public void TriggerAction()
