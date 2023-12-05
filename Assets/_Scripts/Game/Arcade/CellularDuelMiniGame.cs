23401152cb0c453fbccb5cf8e8ae2a3a507756de
Garrett Milliron <gradies@gmail.com>
2023-12-05 18:02:28 -0500

    Intensity scaling on CellularDuel derived games, and tuning on CatNMouse training game

--- Assets/_Scripts/Game/Arcade/CellularDuelMiniGame.cs
+++ Assets/_Scripts/Game/Arcade/CellularDuelMiniGame.cs
@@ -1,4 +1,15 @@
+using CosmicShore.Game.AI;
+using UnityEngine;
+
 namespace CosmicShore.Game.Arcade
 {
-    public class CellularDuelMiniGame : MiniGame { }
+    public class CellularDuelMiniGame : MiniGame 
+    {
+        [SerializeField] Player hostilePilot;
+        protected override void Start()
+        {
+            base.Start();
+            hostilePilot.Ship.GetComponent<AIPilot>().SkillLevel = .4f + (IntensityLevel*.15f);
+        }
+    }
 }
