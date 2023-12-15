6a3d23d4dd6719632125e97e8f1067f6e8f13c37
KidIgarus <isaiah.merrill@gmail.com>
2023-12-14 21:24:21 -0800

    Cut slip&stride block count in half.

--- Assets/_Scripts/Game/Environment/MiniGameObjects/SpawnableHelix.cs
+++ Assets/_Scripts/Game/Environment/MiniGameObjects/SpawnableHelix.cs
@@ -18,7 +18,7 @@ public class SpawnableHelix : SpawnableAbstractBase
 
         var trail = new Trail();
 
-        blockCount = 150;
+        //blockCount = 150;
         var xc1 = Random.Range(4, 16);
         var xc2 = Random.Range(.2f, 2);
         var xc3 = Random.Range(-5, 5);
