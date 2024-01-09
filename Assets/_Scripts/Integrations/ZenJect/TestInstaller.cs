b420e5f840842767b406973b565492eda4aefba1
Echo Yin <echo_ybl@hotmail.com>
2024-01-09 17:17:00 -0500

    Serializable on setting wrapper for installers

--- Assets/_Scripts/Integrations/ZenJect/TestInstaller.cs
+++ Assets/_Scripts/Integrations/ZenJect/TestInstaller.cs
@@ -1,51 +1,14 @@
-using NLog;
 using Zenject;
-
 
 namespace CosmicShore.Integrations.ZenJect
 {
     public class TestInstaller : MonoInstaller
     {
+        public Settings SceneSettings;
         public override void InstallBindings()
         {
             BaseInstaller.Install(Container);
-        }
-    }
-
-    public interface ITestRunner
-    {
-    }
-
-    public class TestRunner : ITestRunner
-    {
-        private ILogger _log;
-        public TestRunner(ILogger log, string message)
-        {
-            _log = log;
-            
-            if (_log.IsInfoEnabled)
-            {
-                _log.Info(message + GetHashCode());
-            }
-        }
-    }
-
-    public class BaseInstaller : Installer<BaseInstaller>
-    {
-        public override void InstallBindings()
-        {
-            var log = LogManager.GetLogger("Logger"); 
-            
-            Container.BindInstance("Hello world").AsSingle();
-            // Container.BindInstance(log).AsSingle();
-            // Container.Bind<NLogManager>().AsSingle().NonLazy();
-            Container.Bind<ILogger>().FromMethod(GetLogger).AsSingle().NonLazy();
-            Container.Bind<ITestRunner>().To<TestRunner>().AsCached().NonLazy();
-        }
-        
-        private ILogger GetLogger(InjectContext context)
-        {
-            return LogManager.GetLogger("Logger");
+            Container.BindInstance(SceneSettings.ShipState);
         }
     }
 }
---
+++ Assets/_Scripts/Integrations/ZenJect/TestRunner.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+
+namespace CosmicShore.Integrations.ZenJect
+{
+    public class TestRunner : ITestRunner
+    {
+        private ILogger _log;
+        public TestRunner(ILogger log, string message)
+        {
+            _log = log;
+            
+            if (_log.IsInfoEnabled)
+            {
+                _log.Info(message + GetHashCode());
+            }
+        }
+    }
+}
