b3e344bd1df9dfe623453ab9b55f44f9082c5266
Echo Yin <echo_ybl@hotmail.com>
2024-01-21 20:13:36 -0500

    Multiple entry points and interface binding

--- Assets/_Scripts/Integrations/VContainer/TestLifetimeScope.cs
+++ Assets/_Scripts/Integrations/VContainer/TestLifetimeScope.cs
@@ -9,16 +9,19 @@ namespace CosmicShore.Integrations.VContainer
         [SerializeField] TestMenu testMenu;
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.RegisterEntryPoint<TestPresenter>();
+            // builder.RegisterEntryPoint<TestPresenter>();
             builder.Register<TestService>(Lifetime.Singleton);
+            builder.Register<IServiceA, TestServiceA>(Lifetime.Singleton);
+            builder.Register<IServiceB, TestServiceB>(Lifetime.Singleton);
+            builder.Register<TestComponentA>(Lifetime.Singleton);
             builder.RegisterComponent(testMenu);
 
             // Use registered entry point with a group
-            // builder.UseEntryPoints(Lifetime.Singleton, entryPoints =>
-            // {
-            //     entryPoints.Add<TestPresenter>();
-            //     // entryPoints.Add<TestPresenter1>();
-            // });
+            builder.UseEntryPoints(Lifetime.Singleton, entryPoints =>
+            {
+                entryPoints.Add<TestPresenter>();
+                entryPoints.Add<ModuleA>();
+            });
         }
     }
 }
