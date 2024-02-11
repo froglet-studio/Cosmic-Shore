8880e6d1ca9b5181cb4d5bb7808111cfa40b5172
Echo Yin <echo_ybl@hotmail.com>
2024-02-10 20:36:43 -0500

    Add generic rewind

---
+++ Assets/_Scripts/App/Systems/RewindSystem/GenericRewind.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+namespace CosmicShore.App.Systems.RewindSystem
+{
+    public class GenericRewind : RewindBase
+    {
+        [Tooltip("Tracking active state of the object that this script is attached to")]
+        [SerializeField]
+        private bool trackObjectActiveState;
+        [Tooltip("Tracking Position,Rotation and Scale")]
+        [SerializeField]
+        private bool trackTransform;
+        [SerializeField] private bool trackVelocity;
+        [SerializeField] private bool trackAnimator;
+        [SerializeField] private bool trackAudio;
+
+        [Tooltip("Enable checkbox on right side to track particles")]
+        // [SerializeField] OptionalParticleSettings trackParticles;
+
+        public override void Rewind(float seconds)
+        {
+            if (trackObjectActiveState)
+                RestoreObjectActiveState(seconds);
+            if (trackTransform)
+                RestoreTransform(seconds);
+            if (trackVelocity)
+                RestoreVelocity(seconds);
+            if (trackAnimator)
+                RestoreAnimator(seconds);
+            if (trackAudio)
+                RestoreAudio(seconds);
+            // if (trackParticles.Enabled)
+            //     RestoreParticles(seconds);       
+        }
+
+        public override void Track()
+        {
+            if (trackObjectActiveState)
+                TrackObjectActiveState();
+            if (trackTransform)
+                TrackTransform();
+            if (trackVelocity)
+                TrackVelocity();
+            if (trackAnimator)
+                TrackAnimator();
+            if (trackAudio)
+                TrackAudio();
+            // if (trackParticles.Enabled)
+            //     TrackParticles();      
+        }
+        // private void Start()
+        // {
+            // if(trackParticles.Enabled)
+            //     InitializeParticles(trackParticles.Value);
+        // }
+    }
+}
