34e186e99faffb29b50c950d25741cef58f738a9
Garrett Milliron <gradies@gmail.com>
2024-04-23 10:33:00 -0400

    branchingFlora progress

--- Assets/_Scripts/Game/Environment/FloraAndFauna/BranchingFlora.cs
+++ Assets/_Scripts/Game/Environment/FloraAndFauna/BranchingFlora.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace CosmicShore
 {
     public class BranchingFlora : Flora
     {
-        [SerializeField] float branchProbability = 0.2f;
+        [SerializeField] float growthChance = 0.2f;
         [SerializeField] float minBranchAngle = 20f;
         [SerializeField] float maxBranchAngle = 40f;
-        [SerializeField] int minBranches = 1;
+        [SerializeField] int minBranches = 1; // don't set to 0
         [SerializeField] int maxBranches = 3;
         [SerializeField] float minBranchLengthMultiplier = 0.6f;
         [SerializeField] float maxBranchLengthMultiplier = 0.8f;
         [SerializeField] int maxDepth = 3;
         [SerializeField] int maxTotalSpawnedObjects = 1000;
-        [SerializeField] List<GameObject> branchPrefabs;
+        [SerializeField] float leafChance = 0.1f;
+        bool endbranch = false;
+
+        HashSet<Branch> activeBranches = new HashSet<Branch>();
+
+        enum BranchType 
+        {
+            HealthBlock,
+            Spindle,
+        }
+
+        struct Branch
+        {
+            public GameObject gameObject;
+            public int depth;
+        }
 
         private int spawnedItemCount = 0;
         private Vector3 currentDisplacement;
@@ -26,109 +42,61 @@ namespace CosmicShore
             base.Start();
             currentDisplacement = transform.position;
             currentRotation = Quaternion.identity;
+            activeBranches.Add(new Branch { gameObject = gameObject, depth = 0 });
         }
 
         public override void Grow()
         {
-            if (spawnedItemCount >= maxTotalSpawnedObjects)
-                return;
+            Debug.Log("Growing");
+            if (spawnedItemCount >= maxTotalSpawnedObjects) return;
 
-            if (Random.value < branchProbability && maxDepth > 0)
+            List<Branch> newBranches = new List<Branch>();
+            List<Branch> branchesToRemove = new List<Branch>();
+
+            foreach (Branch branch in activeBranches)
             {
-                int numBranches = Random.Range(minBranches, maxBranches + 1);
-
-                for (int i = 0; i < numBranches; i++)
+                Debug.Log("Considering Branching");
+                if (Random.value < growthChance && branch.depth < maxDepth)
                 {
-                    float branchAngle = Random.Range(minBranchAngle, maxBranchAngle);
-                    float branchAngleRad = branchAngle * Mathf.Deg2Rad;
-
-                    Vector3 branchDirection = Quaternion.Euler(0f, branchAngleRad * Mathf.Rad2Deg, 0f) * currentRotation * Vector3.forward;
-
-                    float branchLengthMultiplier = Random.Range(minBranchLengthMultiplier, maxBranchLengthMultiplier);
-                    float branchLength = healthBlock.transform.localScale.z * branchLengthMultiplier; // TODO: decrease with depth
-
-                    GameObject branch = SpawnRandomBranch();
-                    branch.transform.position = currentDisplacement + branchDirection * branchLength;
-                    branch.transform.rotation = Quaternion.LookRotation(branchDirection);
-
-                    BranchingFlora branchingFlora = branch.GetComponent<BranchingFlora>();
-                    if (branchingFlora != null)
+                    Branch newBranch = new Branch();
+                    if (Random.value < leafChance)
                     {
-                        branchingFlora.GrowBranches(maxDepth - 1, branchDirection, branchLength);
+                        Debug.Log("Leafing");
+                        newBranch.gameObject = Instantiate(healthBlock, branch.gameObject.transform.position, branch.gameObject.transform.rotation).gameObject; // TODO: position and orient leaf
+                        newBranch.depth = branch.depth + 1;
+                        spawnedItemCount++;
                     }
+                    else
+                    {
+                        Debug.Log("Branching");
+                        int numBranches = Random.Range(minBranches, maxBranches + 1);
+                        for (int i = 0; i < numBranches; i++)
+                        {
+                            float branchAngle = Random.Range(minBranchAngle, maxBranchAngle);
+                            float branchAngleRad = branchAngle * Mathf.Deg2Rad;
+                            Vector3 branchDirection = Quaternion.Euler(0f, branchAngleRad * Mathf.Rad2Deg, 0f) * branch.gameObject.transform.forward;
+                            float branchLengthMultiplier = Random.Range(minBranchLengthMultiplier, maxBranchLengthMultiplier);
+                            float branchLength = branch.depth == 0 ? branchLengthMultiplier : branchLengthMultiplier / branch.depth;
+                            newBranch.gameObject = Instantiate(spindle, branch.gameObject.transform.position, branch.gameObject.transform.rotation).gameObject;
+                            newBranch.gameObject.transform.position += branchDirection * branchLength;
+                            newBranch.gameObject.transform.rotation = Quaternion.LookRotation(branchDirection);
+                            newBranch.depth = branch.depth + 1;
+                            spawnedItemCount++;
+                            newBranches.Add(newBranch);
+                        }
+                    }
+                    branchesToRemove.Add(branch);
                 }
             }
-            else
+
+            foreach (Branch branch in branchesToRemove)
             {
-                SpawnHealthBlock();
+                activeBranches.Remove(branch);
             }
 
-            void SpawnHealthBlock()
-            {
-                Quaternion rotation;
-                currentDisplacement += RandomVectorRotation(healthBlock.transform.localScale.z * Vector3.forward, out rotation);
-                currentRotation = rotation;
-
-                HealthBlock newHealthBlock = Instantiate(healthBlock, currentDisplacement, currentRotation);
-                newHealthBlock.LifeForm = this;
-                AddHealthBlock(newHealthBlock);
-
-                spawnedItemCount++;
-            }
-
-            Quaternion rotation;
-            currentDisplacement += RandomVectorRotation(healthBlock.transform.localScale.z * Vector3.forward, out rotation);
-            currentRotation = rotation;
-
-            HealthBlock newHealthBlock = Instantiate(healthBlock, currentDisplacement, currentRotation);
-            newHealthBlock.LifeForm = this;
-            AddHealthBlock(newHealthBlock);
-
-            spawnedItemCount++;
+            activeBranches.UnionWith(newBranches);
         }
 
-        private void GrowBranches(int depth, Vector3 direction, float length)
-        {
-            if (depth <= 0 || spawnedItemCount >= maxTotalSpawnedObjects)
-                return;
-
-            if (Random.value < branchProbability)
-            {
-                int numBranches = Random.Range(minBranches, maxBranches + 1);
-
-                for (int i = 0; i < numBranches; i++)
-                {
-                    float branchAngle = Random.Range(minBranchAngle, maxBranchAngle);
-                    float branchAngleRad = branchAngle * Mathf.Deg2Rad;
-
-                    Vector3 branchDirection = Quaternion.Euler(0f, branchAngleRad * Mathf.Rad2Deg, 0f) * direction;
-
-                    float branchLengthMultiplier = Random.Range(minBranchLengthMultiplier, maxBranchLengthMultiplier);
-                    float branchLength = length * branchLengthMultiplier;
-
-                    GameObject branch = SpawnRandomBranch();
-                    branch.transform.position = currentDisplacement + branchDirection * branchLength;
-                    branch.transform.rotation = Quaternion.LookRotation(branchDirection);
-
-                    BranchingFlora branchingFlora = branch.GetComponent<BranchingFlora>();
-                    if (branchingFlora != null)
-                    {
-                        branchingFlora.GrowBranches(depth - 1, branchDirection, branchLength);
-                    }
-                }
-            }
-        }
-
-        private GameObject SpawnRandomBranch()
-        {
-            int randomIndex = Random.Range(0, branchPrefabs.Count);
-            GameObject branchPrefab = branchPrefabs[randomIndex];
-
-            GameObject branch = Instantiate(branchPrefab, transform);
-            spawnedItemCount++;
-
-            return branch;
-        }
 
         private Vector3 RandomVectorRotation(Vector3 vector, out Quaternion rotation)
         {
