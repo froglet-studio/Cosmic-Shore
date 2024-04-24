0f715d353d78b8cfc088e1ff382cbfc7c4cfa312
Garrett Milliron <gradies@gmail.com>
2024-04-23 21:55:31 -0400

    branching flora progress

--- Assets/_Scripts/Game/Environment/FloraAndFauna/BranchingFlora.cs
+++ Assets/_Scripts/Game/Environment/FloraAndFauna/BranchingFlora.cs
@@ -8,24 +8,18 @@ namespace CosmicShore
     public class BranchingFlora : Flora
     {
         [SerializeField] float growthChance = 0.2f;
-        [SerializeField] float minBranchAngle = 20f;
+        [SerializeField] float minBranchAngle = -40f;
         [SerializeField] float maxBranchAngle = 40f;
         [SerializeField] int minBranches = 1; // don't set to 0
-        [SerializeField] int maxBranches = 3;
+        [SerializeField] int maxBranches = 2;
+
         [SerializeField] float minBranchLengthMultiplier = 0.6f;
         [SerializeField] float maxBranchLengthMultiplier = 0.8f;
-        [SerializeField] int maxDepth = 3;
+        [SerializeField] int maxDepth = 10;
         [SerializeField] int maxTotalSpawnedObjects = 1000;
-        [SerializeField] float leafChance = 0.1f;
-        bool endbranch = false;
+        [SerializeField] float leafChance = 0.05f;
 
         HashSet<Branch> activeBranches = new HashSet<Branch>();
-
-        enum BranchType 
-        {
-            HealthBlock,
-            Spindle,
-        }
 
         struct Branch
         {
@@ -34,20 +28,15 @@ namespace CosmicShore
         }
 
         private int spawnedItemCount = 0;
-        private Vector3 currentDisplacement;
-        private Quaternion currentRotation;
 
         protected override void Start()
         {
             base.Start();
-            currentDisplacement = transform.position;
-            currentRotation = Quaternion.identity;
             activeBranches.Add(new Branch { gameObject = gameObject, depth = 0 });
         }
 
         public override void Grow()
         {
-            Debug.Log("Growing");
             if (spawnedItemCount >= maxTotalSpawnedObjects) return;
 
             List<Branch> newBranches = new List<Branch>();
@@ -55,13 +44,11 @@ namespace CosmicShore
 
             foreach (Branch branch in activeBranches)
             {
-                Debug.Log("Considering Branching");
                 if (Random.value < growthChance && branch.depth < maxDepth)
                 {
                     Branch newBranch = new Branch();
                     if (Random.value < leafChance)
                     {
-                        Debug.Log("Leafing");
                         newBranch.gameObject = Instantiate(healthBlock, branch.gameObject.transform.position, branch.gameObject.transform.rotation).gameObject; // TODO: position and orient leaf
                         newBranch.depth = branch.depth + 1;
                         spawnedItemCount++;
@@ -74,15 +61,23 @@ namespace CosmicShore
                         {
                             float branchAngle = Random.Range(minBranchAngle, maxBranchAngle);
                             float branchAngleRad = branchAngle * Mathf.Deg2Rad;
-                            Vector3 branchDirection = Quaternion.Euler(0f, branchAngleRad * Mathf.Rad2Deg, 0f) * branch.gameObject.transform.forward;
+                            Vector3 branchDirection = Quaternion.Euler(0f, branchAngleRad * Mathf.Rad2Deg, 0f) * (branch.gameObject.transform.forward);
                             float branchLengthMultiplier = Random.Range(minBranchLengthMultiplier, maxBranchLengthMultiplier);
-                            float branchLength = branch.depth == 0 ? branchLengthMultiplier : branchLengthMultiplier / branch.depth;
-                            newBranch.gameObject = Instantiate(spindle, branch.gameObject.transform.position, branch.gameObject.transform.rotation).gameObject;
-                            newBranch.gameObject.transform.position += branchDirection * branchLength;
+                            //float branchLength = branch.depth == 0 ? branchLengthMultiplier * spindle.Length :
+                            //                                         branchLengthMultiplier * spindle.Length;// / branch.depth;
+                            Debug.Log($"branch depth {branch.depth}");
+                            Debug.Log($"spindleLength {spindle.Length}");
+                            newBranch.gameObject = Instantiate(spindle, branch.gameObject.transform.position + (spindle.cylinder.transform.localScale.y * branch.gameObject.transform.forward), branch.gameObject.transform.rotation).gameObject;
+                            newBranch.gameObject.transform.position = branch.depth < 1 ? branch.gameObject.transform.position + (spindle.cylinder.transform.localScale.y * branch.gameObject.transform.forward) :
+                                                                                               branch.gameObject.transform.position + (spindle.cylinder.transform.localScale.y / (branch.depth - 1) * branch.gameObject.transform.forward) ;
+                            newBranch.gameObject.transform.localScale = branch.depth == 0 ? spindle.transform.localScale :
+                                                                                             spindle.transform.localScale / branch.depth;
+                            newBranch.gameObject.transform.parent = branch.gameObject.transform;
                             newBranch.gameObject.transform.rotation = Quaternion.LookRotation(branchDirection);
                             newBranch.depth = branch.depth + 1;
                             spawnedItemCount++;
                             newBranches.Add(newBranch);
+                            leafChance += 0.01f;
                         }
                     }
                     branchesToRemove.Add(branch);
