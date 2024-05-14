119a9f92448ba47ad577f6e0d26e426309d37904
Echo Yin <echo_ybl@hotmail.com>
2024-05-13 20:17:57 -0600

    Test to PlayFab Sandbox, handle error through utility

--- Assets/_Scripts/Integrations/Playfab/CloudScripts/AzureFunctionExample.cs
+++ Assets/_Scripts/Integrations/Playfab/CloudScripts/AzureFunctionExample.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using CosmicShore.Integrations.Playfab.Authentication;
+using CosmicShore.Integrations.Playfab.Utility;
 using PlayFab;
 using PlayFab.CloudScriptModels;
 using UnityEngine;
@@ -22,31 +23,33 @@ namespace CosmicShore.Integrations.Playfab.CloudScripts
         {
             var entityId = AuthenticationManager.PlayFabAccount.AuthContext.EntityId;
             var entityType = AuthenticationManager.PlayFabAccount.AuthContext.EntityType;
-            
-            PlayFabCloudScriptAPI.ExecuteFunction(new ExecuteFunctionRequest
+
+            var request =
+                new ExecuteFunctionRequest //Set this to true if you would like this call to show up in PlayStream
             {
-                
                 Entity = new EntityKey
                 {
                     Id = entityId, //Get this from when you logged in,
                     Type = entityType //Get this from when you logged in
                 },
                 FunctionName = "HelloWorld", //This should be the name of your Azure Function that you created.
-                FunctionParameter = new Dictionary<string, object> { { "inputValue", "Test" } }, //This is the data that you would want to pass into your function.
+                FunctionParameter = new Dictionary<string, object>
+                    { { "inputValue", "Test" } }, //This is the data that you would want to pass into your function.
                 GeneratePlayStreamEvent = false //Set this to true if you would like this call to show up in PlayStream
-            }, result =>
+            };
+
+            PlayFabCloudScriptAPI.ExecuteFunction(request, OnHelloWorldSuccess, PlayFabUtility.HandleErrorReport);
+        }
+
+        private void OnHelloWorldSuccess(ExecuteFunctionResult result)
+        {
+            if (result.FunctionResultTooLarge ?? false)
             {
-                if (result.FunctionResultTooLarge ?? false)
-                {
-                    Debug.Log("Cloud script - This can happen if you exceed the limit that can be returned from an Azure Function, See PlayFab Limits Page for details.");
-                    return;
-                }
-                Debug.Log($"Cloud script - The {result.FunctionName} function took {result.ExecutionTimeMilliseconds} to complete");
-                Debug.Log($"Cloud script - Result: {result.FunctionResult}");
-            }, error =>
-            {
-                Debug.LogError($"Cloud script - Something went wrong: {error.GenerateErrorReport()}");
-            });
+                Debug.Log("Cloud script - This can happen if you exceed the limit that can be returned from an Azure Function, See PlayFab Limits Page for details.");
+                return;
+            }
+            Debug.Log($"Cloud script - The {result.FunctionName} function took {result.ExecutionTimeMilliseconds} to complete");
+            Debug.Log($"Cloud script - Result: {result.FunctionResult}");
         }
     }
 }
