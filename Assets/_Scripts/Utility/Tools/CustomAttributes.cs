7dc13a7b88c461165cdb3135906020001cbedc3c
Emmanuel Eytan <list@regularoddity.com>
2024-05-20 14:39:53 -0700

    Added docs. They should have been written earlier. Oops.

--- Assets/_Scripts/Utility/Tools/CustomAttributes.cs
+++ Assets/_Scripts/Utility/Tools/CustomAttributes.cs
@@ -7,8 +7,18 @@ using Mono.CSharp;
 using System.Linq;
 using QFSW.QC.Utilities;
 
+
 namespace CosmicShore.Utility.Attributes
 {
+	/// <summary>
+	/// A class-only attribute that adds the name of a minigame as metadata.
+	/// <example>
+	/// [MinigameName("FunGame")]
+	/// public class FunGame: Minigame
+	/// {
+	///    /* ... */
+	/// }
+	/// </summary>
 	[AttributeUsage(System.AttributeTargets.Class)]
 	class MinigameNameAttribute : System.Attribute
 	{
@@ -19,9 +29,21 @@ namespace CosmicShore.Utility.Attributes
 			Name = name;
 		}
 	}
-	
+
+	/// <summary>
+	/// A Unity post-processor that will run every time the asset library is refreshed
+	/// and look for every instance of the MinigameName attribute.
+	/// </summary>
 	class MetedataWriter: AssetPostprocessor
 	{
+		/// <summary>
+		/// An implementetion of the <c cref="https://docs.unity3d.com/2021.3/Documentation/ScriptReference/AssetPostprocessor">AssetPostprocessor</c>'s
+		/// <c cref="https://docs.unity3d.com/2021.3/Documentation/ScriptReference/AssetPostprocessor.OnPostprocessAllAssets.html">OnPostprocessAllAssets</c> member.
+		/// Looks for every class that extends <c cref="CosmicShore.Game.Arcade.MiniGame"/> (but not <c>Minigame</c> itself) and
+		/// collects all of the <c>name</c> arguments added to them through instances of the <c cref="MinigameNameAttribute" /> attribute.
+		///
+		/// None of the arguments of the method are used in this implementation.
+		/// </summary>
 		static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
 		{
 			Type[] minigameTypes = typeof(MiniGame).Assembly.GetTypes();
