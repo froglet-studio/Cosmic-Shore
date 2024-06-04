bb47e0e9605ac356500cdf71ee99a5d80f306d5f
KidIgarus <isaiah.merrill@gmail.com>
2024-06-03 19:31:19 -0700

    Label ElementalFloat properties in the inspector with the name of the property in script

--- Assets/_Scripts/Editor/ElementalFloatDrawer.cs
+++ Assets/_Scripts/Editor/ElementalFloatDrawer.cs
@@ -14,11 +14,12 @@ public class ElementalFloatDrawer : PropertyDrawer
         EditorGUI.BeginProperty(position, label, property);
 
         // Calculate rects
-        Rect enabledRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
-        Rect valueRect = new Rect(position.x, position.y + 18, position.width, EditorGUIUtility.singleLineHeight);
-        Rect minRect = new Rect(position.x, position.y + 36, position.width, EditorGUIUtility.singleLineHeight);
-        Rect maxRect = new Rect(position.x, position.y + 54, position.width, EditorGUIUtility.singleLineHeight);
-        Rect elementRect = new Rect(position.x, position.y + 72, position.width, EditorGUIUtility.singleLineHeight);
+        Rect nameRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+        Rect enabledRect = new Rect(position.x, position.y + 18, position.width, EditorGUIUtility.singleLineHeight);
+        Rect valueRect = new Rect(position.x, position.y + 36, position.width, EditorGUIUtility.singleLineHeight);
+        Rect minRect = new Rect(position.x, position.y + 54, position.width, EditorGUIUtility.singleLineHeight);
+        Rect maxRect = new Rect(position.x, position.y + 72, position.width, EditorGUIUtility.singleLineHeight);
+        Rect elementRect = new Rect(position.x, position.y + 90, position.width, EditorGUIUtility.singleLineHeight);
 
         // Draw fields - pass GUIContent.none to each so they are drawn without labels
         SerializedProperty enabledProperty = property.FindPropertyRelative("Enabled");
@@ -27,14 +28,15 @@ public class ElementalFloatDrawer : PropertyDrawer
         SerializedProperty maxProperty = property.FindPropertyRelative("Max");
         SerializedProperty elementProperty = property.FindPropertyRelative("element");
 
-        EditorGUI.PropertyField(enabledRect, enabledProperty, new GUIContent("Enabled"));
-        EditorGUI.PropertyField(valueRect, valueProperty, new GUIContent("Value"));
+        EditorGUI.SelectableLabel(nameRect, $"{property.name} (Elemental Float)", EditorStyles.boldLabel);
+        EditorGUI.PropertyField(enabledRect, enabledProperty, new GUIContent("   Enabled"));
+        EditorGUI.PropertyField(valueRect, valueProperty, new GUIContent("   Value"));
 
         if (enabledProperty.boolValue)
         {
-            EditorGUI.PropertyField(minRect, minProperty, new GUIContent("Min"));
-            EditorGUI.PropertyField(maxRect, maxProperty, new GUIContent("Max"));
-            EditorGUI.PropertyField(elementRect, elementProperty, new GUIContent("Element"));
+            EditorGUI.PropertyField(minRect, minProperty, new GUIContent("   Min"));
+            EditorGUI.PropertyField(maxRect, maxProperty, new GUIContent("   Max"));
+            EditorGUI.PropertyField(elementRect, elementProperty, new GUIContent("   Element"));
         }
 
         EditorGUI.EndProperty();
@@ -45,11 +47,11 @@ public class ElementalFloatDrawer : PropertyDrawer
         SerializedProperty enabledProperty = property.FindPropertyRelative("Enabled");
         if (enabledProperty.boolValue)
         {
-            return EditorGUIUtility.singleLineHeight * 5 + 8;
+            return EditorGUIUtility.singleLineHeight * 6 + 8;
         }
         else
         {
-            return EditorGUIUtility.singleLineHeight * 2 + 4;
+            return EditorGUIUtility.singleLineHeight * 3 + 4;
         }
     }
 }
