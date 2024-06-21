f62e260e3e87e72b1ce055d47cbb926b2a52bfac
Echo Yin <echo_ybl@hotmail.com>
2024-06-20 19:43:42 -0600

    Test out chain of responsibilities

---
+++ Assets/_Scripts/Integrations/Architectures/ChainResponsibilities/AuthViewTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace CosmicShore.Integrations.Architectures.ChainResponsibilities
+{
+    public class AuthViewTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            // Initiate handlers
+            var authHandler = new AuthenticationHandler();
+            var errorHandler = new ErrorHandler();
+                
+            // Set the responsibility chain
+            authHandler.SetNext(errorHandler);
+                
+            Debug.Log("Chain: Auth -> Error");
+            AuthenticationManagerTest.DoRequests(authHandler);
+        }
+    }
+}
