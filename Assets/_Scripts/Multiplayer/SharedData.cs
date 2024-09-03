3378aa4e5085182edcd68f1cb81767c7e9bdc5af
KidIgarus <isaiah.merrill@gmail.com>
2024-09-03 11:41:02 -0700

    Fix build issue from firebase plugin from redundant .Net dlls.

--- Assets/_Scripts/Multiplayer/SharedData.cs
+++
@@ -1,7 +0,0 @@
-namespace CosmicShore
-{
-    public static class SharedData
-    {
-        public const string HttpUrl = "https://rcp2ozvmp6ntifzfycjklzvlme0aufav.lambda-url.us-west-2.on.aws";
-    }
-}
