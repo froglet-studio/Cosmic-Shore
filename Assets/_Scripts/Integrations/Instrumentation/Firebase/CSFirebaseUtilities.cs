a10ab66193f35d0db5b9caa15f314273cf569b89
Echo Yin <echo_ybl@hotmail.com>
2024-11-22 23:30:16 -0700

    Update instrumentation a bit

---
+++ Assets/_Scripts/Integrations/Instrumentation/Firebase/CSFirebaseUtilities.cs
@@ -0,0 +1,12 @@
+using CosmicShore.Integrations.Instrumentation.Interfaces;
+
+namespace CosmicShore.Integrations.Instrumentation.Firebase
+{
+    public class CSFirebaseUtilities : IAnalyzable
+    {
+        public void InitSDK()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/_Scripts/Integrations/Instrumentation/Firebase/CSStoreDataCollectorFirebase.cs
@@ -0,0 +1,38 @@
+using CosmicShore.Integrations.Instrumentation.Interfaces;
+
+namespace CosmicShore.Integrations.Instrumentation.Firebase
+{
+    public class CSStoreDataCollectorFirebase : IStoreAnalyzable
+    {
+        
+        public void InitSDK()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PurchaseCaptain()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PurchaseArcadeGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PurchaseMission()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WatchAd()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RedeemDailyReward()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
