975ef6773fac86738d072cc8b08a5c8ead149a72
Garrett Milliron <gradies@gmail.com>
2024-12-23 19:57:26 -0500

    FInished removing sqr roots frrom explosions

--- Assets/_Scripts/Utility/Effects/BlockImpact.cs
+++ Assets/_Scripts/Utility/Effects/BlockImpact.cs
@@ -17,18 +17,24 @@ public class BlockImpact : MonoBehaviour
         
         Vector3 distance = Vector3.zero;
         velocity = GeometryUtils.ClampMagnitude(velocity, minSpeed, maxSpeed);
+        var explosionAmount = 0f;
+        var explosionSpeed = Mathf.Max(velocity.sqrMagnitude / maxSpeed, minSpeed);
+
         material = gameObject.GetComponent<MeshRenderer>().material;
+        material.SetVector("_velocity", velocity);
+
         var initialPosition = transform.position;
         var maxDuration = 7;
         var duration = 0f;
+
         while (duration <= maxDuration)
         {
             yield return null;
             duration += Time.deltaTime;
             distance += Time.deltaTime * velocity;
-            material.SetVector("_velocity", distance);
+            explosionAmount += explosionSpeed * Time.deltaTime;
+            material.SetFloat("_ExplosionAmount", explosionAmount);
             material.SetFloat("_opacity", 1 - (duration / maxDuration));
-          
             transform.position = initialPosition + distance;
         }
 
