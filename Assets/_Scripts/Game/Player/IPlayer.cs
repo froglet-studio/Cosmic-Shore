694d6183b9080c340125eaf490c14e1320cafe37
Yash Sadhukhan <yskhan61@gmail.com>
2025-01-11 00:19:58 +0530

    Add IPlayer interface

---
+++ Assets/_Scripts/Game/Player/IPlayer.cs
@@ -0,0 +1,35 @@
+﻿using CosmicShore.Game.IO;
+using CosmicShore.Game.UI;
+using CosmicShore.Utility;
+
+
+namespace CosmicShore.Game
+{
+    public interface IPlayer : ITransform
+    {
+        public ShipTypes DefaultShipType { get; }
+        public Teams Team { get; }
+        public string PlayerName { get; }
+        public string PlayerUUID { get; }
+        public string Name { get; }
+        public IShip Ship { get; }
+        public InputController InputController { get; }
+        public GameCanvas GameCanvas { get; }
+        public bool IsActive { get; }
+
+        public void Initialize(InitializeData data);
+        public void ToggleActive(bool active);
+        public void SetDefaultShipType(ShipTypes defaultShipType);
+        public void ToggleGameObject(bool toggle);
+
+
+        public struct InitializeData
+        {
+            public ShipTypes DefaultShipType;
+            public Teams Team;
+            public string PlayerName;
+            public string PlayerUUID;
+            public string Name;
+        }
+    }
+}
