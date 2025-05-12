b9be6a189fcd5c9f465b0609ef87b4d133226ba4
Garrett Milliron <gradies@gmail.com>
2025-05-12 14:11:02 -0400

    Squirrel boosters changes: now spaced out chevrons

--- Assets/_Scripts/Game/Environment/Cytoplasm/NudgeShard.cs
+++ Assets/_Scripts/Game/Environment/Cytoplasm/NudgeShard.cs
@@ -8,8 +8,8 @@ namespace CosmicShore
 {
     public class NudgeShard : MonoBehaviour
     {
-        float Displacement = 1f;
-        float Duration = .2f;
+        float Displacement = 75f;
+        float Duration = .75f;
         [SerializeField] int boostResourceIndex = 0;
         [SerializeField] float MaxBoostMultiplier = 2;
         [SerializeField] float BoostDischargeRate = .25f;
@@ -27,42 +27,43 @@ namespace CosmicShore
         {
             if (other.gameObject.IsLayer("Ships"))
             {
-                if (!other.TryGetComponent(out IShipStatus shipStatus))
+                if (!other.TryGetComponent(out ShipGeometry shipGeometry))
                 {
                     return;
                 }
-
-                if (shipStatus.ShipType == ShipTypes.Squirrel)
-                {
-                    //var sign = Mathf.Sign(Vector3.Dot(transform.parent.forward, ship.Transform.forward));
-                    //ship.ShipTransformer.ModifyVelocity(sign * transform.parent.forward * Displacement, Duration);
-                    StartCoroutine(DischargeBoostCoroutine(shipStatus, Displacement));
-                    Prism?.Steal(shipStatus.Player);
-                }
+                
+                //if (shipStatus.ShipType == ShipTypes.Squirrel)
+                //{
+                //    //var sign = Mathf.Sign(Vector3.Dot(transform.parent.forward, ship.Transform.forward));
+                //    //ship.ShipTransformer.ModifyVelocity(sign * transform.parent.forward * Displacement, Duration);
+                //    StartCoroutine(DischargeBoostCoroutine(shipStatus, Displacement));
+                //    Prism?.Steal(shipStatus.Player);
+                //}
                 else
                 {
+                    var shipStatus = shipGeometry.Ship.ShipStatus;
                     shipStatus.ShipTransformer.ModifyVelocity(transform.parent.forward * Displacement, Duration);
                 }
             }
         }
 
-        IEnumerator DischargeBoostCoroutine(IShipStatus shipStatus, float ChargedBoostIncrease)
-        {
-            // TODO: figure out how to get ship data component here so that it is not null
-            shipStatus.ResourceSystem.ChangeResourceAmount(boostResourceIndex, ChargedBoostIncrease);
-            shipStatus.ChargedBoostCharge += ChargedBoostIncrease;
-            shipStatus.ChargedBoostDischarging = true;
-            while (shipStatus.ChargedBoostCharge > 1)
-            {
-                shipStatus.ResourceSystem.ChangeResourceAmount(boostResourceIndex, -BoostDischargeRate);
-                shipStatus.ChargedBoostCharge = 1 + (MaxBoostMultiplier * shipStatus.ResourceSystem.Resources[boostResourceIndex].CurrentAmount);
-                yield return new WaitForSeconds(.1f);
-            }
-            shipStatus.ChargedBoostCharge = 1;
-            shipStatus.ChargedBoostDischarging = false;
+        //IEnumerator DischargeBoostCoroutine(IShipStatus shipStatus, float ChargedBoostIncrease)
+        //{
+        //    // TODO: figure out how to get ship data component here so that it is not null
+        //    shipStatus.ResourceSystem.ChangeResourceAmount(boostResourceIndex, ChargedBoostIncrease);
+        //    shipStatus.ChargedBoostCharge += ChargedBoostIncrease;
+        //    shipStatus.ChargedBoostDischarging = true;
+        //    while (shipStatus.ChargedBoostCharge > 1)
+        //    {
+        //        shipStatus.ResourceSystem.ChangeResourceAmount(boostResourceIndex, -BoostDischargeRate/Duration);
+        //        shipStatus.ChargedBoostCharge = 1 + (MaxBoostMultiplier * shipStatus.ResourceSystem.Resources[boostResourceIndex].CurrentAmount);
+        //        yield return new WaitForSeconds(.1f);
+        //    }
+        //    shipStatus.ChargedBoostCharge = 1;
+        //    shipStatus.ChargedBoostDischarging = false;
 
-            shipStatus.ResourceSystem.ChangeResourceAmount(boostResourceIndex, -shipStatus.ResourceSystem.Resources[boostResourceIndex].CurrentAmount);
-        }
+        //    shipStatus.ResourceSystem.ChangeResourceAmount(boostResourceIndex, -shipStatus.ResourceSystem.Resources[boostResourceIndex].CurrentAmount);
+        //}
 
     }
 }
