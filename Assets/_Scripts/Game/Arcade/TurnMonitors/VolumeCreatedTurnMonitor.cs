0ae114d437495e4a0bedd1af55572991cef446fb
Yash Sadhukhan <yskhan61@gmail.com>
2025-06-05 17:43:07 +0530

    Refactor -> StatsManager
    Remove StatsManager component from all scenes and add the StatsManager prefab for better organisation.

--- Assets/_Scripts/Game/Arcade/TurnMonitors/VolumeCreatedTurnMonitor.cs
+++ Assets/_Scripts/Game/Arcade/TurnMonitors/VolumeCreatedTurnMonitor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using CosmicShore.Core;
 
 namespace CosmicShore.Game.Arcade
 {
---
+++ Assets/_Scripts/Game/Managers/NetworkStatsManager.cs
@@ -0,0 +1,58 @@
+﻿using Unity.Multiplayer.Samples.Utilities;
+using UnityEngine;
+
+
+namespace CosmicShore.Core
+{
+    [RequireComponent(typeof(NetcodeHooks))]
+    public class NetworkStatsManager : StatsManager
+    {
+        NetcodeHooks _netcodeHooks;
+
+        public override void Awake()
+        {
+            base.Awake();
+            _netcodeHooks = GetComponent<NetcodeHooks>();
+        }
+
+        protected override void OnEnable()
+        {
+            GameManager.OnPlayGame += ResetStats;
+            GameManager.OnGameOver += OutputRoundStats;
+
+
+            _netcodeHooks.OnNetworkSpawnHook += OnNetworkSpawn;
+            _netcodeHooks.OnNetworkDespawnHook += OnNetworkDespawn;
+        }
+
+        protected override void OnDisable()
+        {
+            GameManager.OnPlayGame -= ResetStats;
+            GameManager.OnGameOver -= OutputRoundStats;
+
+            _netcodeHooks.OnNetworkSpawnHook -= OnNetworkSpawn;
+            _netcodeHooks.OnNetworkDespawnHook -= OnNetworkDespawn;
+        }
+
+        private void OnNetworkSpawn()
+        {
+            if (_netcodeHooks.IsServer)
+            {
+                _onTrailBlockCreatedEventChannel.OnEventRaised += OnBlockCreated;
+                _onTrailBlockDestroyedEventChannel.OnEventRaised += OnBlockDestroyed;
+                _onTrailBlockRestoredEventChannel.OnEventRaised += OnBlockRestored;
+            }
+        }
+
+        private void OnNetworkDespawn()
+        {
+            if (_netcodeHooks.IsServer)
+            {
+                _onTrailBlockCreatedEventChannel.OnEventRaised -= OnBlockCreated;
+                _onTrailBlockDestroyedEventChannel.OnEventRaised -= OnBlockDestroyed;
+                _onTrailBlockRestoredEventChannel.OnEventRaised -= OnBlockRestored;
+            }
+        }
+    }
+}
+
