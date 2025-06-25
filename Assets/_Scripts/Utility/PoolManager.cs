94b867842cabbb18ffd64c63581547453e7421a8
Yash Sadhukhan <yskhan61@gmail.com>
2025-06-25 09:46:06 +0530

    Refactor Scripts

--- Assets/_Scripts/Utility/PoolManager.cs
+++ Assets/_Scripts/Utility/PoolManager.cs
@@ -1,31 +1,5 @@
-using UnityEngine;
 using CosmicShore.Core;
-using System.Collections;
-using CosmicShore.Game;
-using System.Collections.Generic;
-using CosmicShore.Game.Projectiles;
 
 public class PoolManager : PoolManagerBase
 {
-    [SerializeField, RequireInterface(typeof(IShip))] MonoBehaviour shipInstance;
-    IShip Ship;
-
-    protected override void Awake()
-    {
-        Ship = shipInstance as IShip;
-        base.Awake();
-        StartCoroutine(WaitForShipInitialization());
-    }
-
-    IEnumerator WaitForShipInitialization()
-    {
-        // Wait until we have a valid ship reference and its player is initialized
-        while (Ship == null || Ship.ShipStatus.Player == null)
-        {
-            // yield return new WaitForEndOfFrame();
-            yield return null;
-        }
-        
-        transform.parent = Ship.ShipStatus.Player.Transform;
-    }
 }
--- Assets/_Scripts/Utility/TeamColorPersistentPool.cs
+++ Assets/_Scripts/Utility/TeamColorPersistentPool.cs
@@ -1,120 +1,106 @@
 using UnityEngine;
-using System.Collections.Generic;
 using CosmicShore.Core;
 using System.Collections;
+using CosmicShore.Utilities;
+using System;
 
-public class TeamColorPersistentPool : PoolManagerBase
+
+namespace CosmicShore.Game
 {
-    private static TeamColorPersistentPool instance;
-    public static TeamColorPersistentPool Instance
+    public class TeamColorPersistentPool : PoolManagerBase
     {
-        get
+
+        [SerializeField] private GameObject fossilBlockPrefab;
+        [SerializeField] private int poolSizePerTeam = 750; // 750 per team = 3000 total
+
+        [Header("Event Channels")]
+        [SerializeField] TrailBlockEventChannelWithReturnSO _onFlockSpawnedEventChannel;
+
+        [Header("Data Containers")]
+        [SerializeField] ThemeManagerDataContainerSO _themeManagerData;
+
+        private void OnEnable()
         {
-            if (instance == null)
+            _onFlockSpawnedEventChannel.OnEventReturn += OnFlockSpawnedEventRaised;
+        }
+
+        private void OnDisable()
+        {
+            _onFlockSpawnedEventChannel.OnEventReturn -= OnFlockSpawnedEventRaised;
+        }
+
+        private TrailBlockReturnEventData OnFlockSpawnedEventRaised(TrailBlockEventData data)
+        {
+            var spawnedObject = SpawnFromTeamPool(data.Team, data.Position, data.Rotation);
+            return new TrailBlockReturnEventData
             {
-                GameObject go = new GameObject("TeamColorPersistentPool");
-                instance = go.AddComponent<TeamColorPersistentPool>();
-                DontDestroyOnLoad(go);
+                SpawnedObject = spawnedObject
+            };
+        }
+
+        protected override GameObject CreatePoolObject(GameObject prefab)
+        {
+            GameObject obj = base.CreatePoolObject(prefab);
+
+            // Set the material based on the pool's tag
+            var renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                Teams team = Teams.Jade; // Default
+
+                // Determine team from tag
+                if (prefab.tag.Contains("Ruby")) team = Teams.Ruby;
+                else if (prefab.tag.Contains("Gold")) team = Teams.Gold;
+                else if (prefab.tag.Contains("Blue")) team = Teams.Blue;
+
+                // Create and set the material
+                Material teamMaterial = new Material(_themeManagerData.GetTeamExplodingBlockMaterial(team));
+                renderer.material = teamMaterial;
             }
-            return instance;
+
+            return obj;
         }
-    }
 
-    [SerializeField] private GameObject fossilBlockPrefab;
-    [SerializeField] private int poolSizePerTeam = 750; // 750 per team = 3000 total
-
-    protected override void Awake()
-    {
-        if (instance == null)
+        protected override void InitializePoolDictionary()
         {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
+            // Initialize default pool
+            AddConfigData(fossilBlockPrefab, poolSizePerTeam);
 
-            base.Awake();
+            // Temporarily modify the tag for each team
+            string originalTag = fossilBlockPrefab.tag;
 
-            // Initialize all team pools
-            StartCoroutine(WaitForThemeManagerInitialization());
+            fossilBlockPrefab.tag = "FossilPrism_Ruby";
+            AddConfigData(fossilBlockPrefab, poolSizePerTeam);
+
+            fossilBlockPrefab.tag = "FossilPrism_Gold";
+            AddConfigData(fossilBlockPrefab, poolSizePerTeam);
+
+            fossilBlockPrefab.tag = "FossilPrism_Blue";
+            AddConfigData(fossilBlockPrefab, poolSizePerTeam);
+
+            // Restore original tag
+            fossilBlockPrefab.tag = originalTag;
         }
-        else
+
+        public GameObject SpawnFromTeamPool(Teams team, Vector3 position, Quaternion rotation)
         {
-            Destroy(gameObject);
+            string tag = "FossilPrism";
+
+            // Get the appropriate pool based on team
+            switch (team)
+            {
+                case Teams.Ruby:
+                    tag = "FossilPrism_Ruby";
+                    break;
+                case Teams.Gold:
+                    tag = "FossilPrism_Gold";
+                    break;
+                case Teams.Blue:
+                    tag = "FossilPrism_Blue";
+                    break;
+            }
+
+            return SpawnFromPool(tag, position, rotation);
         }
-    }
-
-    protected override GameObject CreatePoolObject(GameObject prefab)
-    {
-        GameObject obj = base.CreatePoolObject(prefab);
-        
-        // Set the material based on the pool's tag
-        var renderer = obj.GetComponent<Renderer>();
-        if (renderer != null)
-        {
-            Teams team = Teams.Jade; // Default
-            
-            // Determine team from tag
-            if (prefab.tag.Contains("Ruby")) team = Teams.Ruby;
-            else if (prefab.tag.Contains("Gold")) team = Teams.Gold;
-            else if (prefab.tag.Contains("Blue")) team = Teams.Blue;
-            
-            // Create and set the material
-            Material teamMaterial = new Material(ThemeManager.Instance.GetTeamExplodingBlockMaterial(team));
-            renderer.material = teamMaterial;
-        }
-        
-        return obj;
-    }
-
-    //the following coroutine waits for the thememanager to initialize before creating the team pools
-    IEnumerator WaitForThemeManagerInitialization()
-    {
-        while (ThemeManager.Instance == null)
-        {
-            yield return new WaitForEndOfFrame();
-        }
-        
-        // Initialize all team pools
-        InitializeTeamPools(fossilBlockPrefab, poolSizePerTeam);
-    }
-
-    private void InitializeTeamPools(GameObject prefab, int sizePerTeam)
-    {
-        // Initialize default pool
-        InitializePool(prefab, sizePerTeam);
-
-        // Temporarily modify the tag for each team
-        string originalTag = prefab.tag;
-
-        prefab.tag = "FossilPrism_Ruby";
-        InitializePool(prefab, sizePerTeam);
-
-        prefab.tag = "FossilPrism_Gold";
-        InitializePool(prefab, sizePerTeam);
-
-        prefab.tag = "FossilPrism_Blue";
-        InitializePool(prefab, sizePerTeam);
-
-        // Restore original tag
-        prefab.tag = originalTag;
-    }
-
-    public GameObject SpawnFromTeamPool(Teams team, Vector3 position, Quaternion rotation)
-    {
-        string tag = "FossilPrism";
-        
-        // Get the appropriate pool based on team
-        switch (team)
-        {
-            case Teams.Ruby:
-                tag = "FossilPrism_Ruby";
-                break;
-            case Teams.Gold:
-                tag = "FossilPrism_Gold";
-                break;
-            case Teams.Blue:
-                tag = "FossilPrism_Blue";
-                break;
-        }
-        
-        return SpawnFromPool(tag, position, rotation);
     }
 }
