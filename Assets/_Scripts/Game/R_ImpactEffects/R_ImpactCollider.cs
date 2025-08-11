aa36e7ceab9b06a469c5ba74f605124520f12969
Yash Sadhukhan <yskhan61@gmail.com>
2025-08-11 14:37:09 +0530

    Fix Prism Impact Effects for Ships

--- Assets/_Scripts/Game/R_ImpactEffects/R_ImpactCollider.cs
+++ Assets/_Scripts/Game/R_ImpactEffects/R_ImpactCollider.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace CosmicShore.Game
 {
+    /// <summary>
+    /// Separating this component from R_IImpactor lets us to get colliders
+    /// </summary>
     public class R_ImpactCollider : MonoBehaviour, R_IImpactCollider
     {
         [SerializeField, RequireInterface(typeof(R_IImpactor))] 
