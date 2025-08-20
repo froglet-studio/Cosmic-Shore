b11276d0537995cab8a85d86e4aef06755679729
Shombith03 <shombithofficial@outlook.com>
2025-08-20 07:13:42 +0530

    Add Mine Prefab + Add Mine Impactor and Mine class

---
+++ Assets/_Scripts/Game/ImpactEffects/MineImpactor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace CosmicShore.Game
+{
+    public class MineImpactor : ImpactorBase
+    {
+        [SerializeField, RequireInterface(typeof(IImpactEffect))]
+        ScriptableObject[] mineShipEffectsSO;
+        
+        [SerializeField, RequireInterface(typeof(IImpactEffect))]
+        ScriptableObject[] mineProjectileEffectsSO;
+        
+        IImpactEffect[] mineShipEffects;
+        IImpactEffect[] mineProjectileEffects;
+        
+        protected virtual void Awake()
+        {
+            mineShipEffects = Array.ConvertAll(mineShipEffectsSO, so => so as IImpactEffect);
+            mineProjectileEffects = Array.ConvertAll(mineProjectileEffectsSO, so => so as IImpactEffect);
+        }
+        
+        protected override void AcceptImpactee(IImpactor impactee)
+        {
+            switch (impactee)
+            {
+                case ShipImpactor shipImpactor:
+                    ExecuteEffect(impactee, mineShipEffects);
+                    break;
+                case ProjectileImpactor projectileImpactor:
+                    ExecuteEffect(impactee, mineProjectileEffects);
+                    break;
+            }
+        }
+    }
+}
