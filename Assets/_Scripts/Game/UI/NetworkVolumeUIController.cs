a398c0dfad5d3b9e70160fa3225a7e7c4c29dc8f
Yash Sadhukhan <yskhan61@gmail.com>
2025-10-04 14:59:33 +0530

    Refactor Volume Display System

---
+++ Assets/_Scripts/Game/UI/NetworkVolumeUIController.cs
@@ -0,0 +1,121 @@
+using CosmicShore.SOAP;
+using Unity.Netcode;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+namespace CosmicShore.Game.UI
+{
+    public class NetworkVolumeUIController : NetworkBehaviour
+    {
+        [SerializeField] MiniGameDataSO miniGameData;
+        [SerializeField] VolumeUI volumeUI;
+
+        private bool _active;
+        private bool _initialized;
+
+        public override void OnNetworkSpawn()
+        {
+            if (IsServer)
+            {
+                miniGameData.OnStarted += OnMiniGameStart_Server;
+                miniGameData.OnMiniGameTurnEnd += OnMiniGameTurnEnd_Server;
+            }
+
+            if (IsClient)
+            {
+                // Late joiners check if the game is already running
+                TrySyncLateJoinStateAsync().Forget();
+            }
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            if (IsServer)
+            {
+                miniGameData.OnStarted -= OnMiniGameStart_Server;
+                miniGameData.OnMiniGameTurnEnd -= OnMiniGameTurnEnd_Server;
+            }
+        }
+
+        private async UniTaskVoid TrySyncLateJoinStateAsync()
+        {
+            // Wait a small moment to ensure MiniGameData is initialized
+            await UniTask.Delay(500, DelayType.UnscaledDeltaTime);
+
+            if (miniGameData.IsRunning)
+            {
+                // Request the current state from server
+                RequestSyncFromServer_ServerRpc();
+            }
+        }
+
+        #region --- SERVER HANDLERS ---
+
+        private void OnMiniGameStart_Server()
+        {
+            _active = true;
+            SendActiveState_ClientRpc(true, miniGameData.GetTeamVolumes());
+        }
+
+        private void OnMiniGameTurnEnd_Server()
+        {
+            _active = false;
+            SendActiveState_ClientRpc(false, Vector4.zero);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void RequestSyncFromServer_ServerRpc(ServerRpcParams rpcParams = default)
+        {
+            var senderId = rpcParams.Receive.SenderClientId;
+            SendActiveState_SingleClientRpc(senderId, _active, miniGameData.GetTeamVolumes());
+        }
+
+        #endregion
+
+        #region --- CLIENT RPCS ---
+
+        [ClientRpc]
+        private void SendActiveState_ClientRpc(bool state, Vector4 teamVolumes)
+        {
+            _active = state;
+            if (_active)
+            {
+                volumeUI.UpdateVolumes(teamVolumes);
+                MonitorVolumesAsync().Forget();
+            }
+        }
+
+        [ClientRpc]
+        private void SendActiveState_SingleClientRpc(ulong targetClientId, bool state, Vector4 teamVolumes, ClientRpcParams clientRpcParams = default)
+        {
+            if (NetworkManager.Singleton.LocalClientId != targetClientId) return;
+            _active = state;
+            if (_active)
+            {
+                volumeUI.UpdateVolumes(teamVolumes);
+                MonitorVolumesAsync().Forget();
+            }
+        }
+
+        #endregion
+
+        private async UniTaskVoid MonitorVolumesAsync()
+        {
+            if (_initialized) return;
+            _initialized = true;
+
+            while (_active && this != null)
+            {
+                if (miniGameData != null && volumeUI != null)
+                {
+                    var teamVolumes = miniGameData.GetTeamVolumes();
+                    volumeUI.UpdateVolumes(teamVolumes);
+                }
+
+                await UniTask.Delay(250, DelayType.UnscaledDeltaTime);
+            }
+
+            _initialized = false;
+        }
+    }
+}
---
+++ Assets/_Scripts/Game/UI/VolumeUI.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CosmicShore.Game.UI
+{
+    [RequireComponent(typeof(Image))]
+    public class VolumeUI : MonoBehaviour
+    {
+        [SerializeField] float upperBound = 300000f;
+
+        private Material _material;
+        private Vector4 _colorRadii;
+
+        void Awake()
+        {
+            var image = GetComponent<Image>();
+            _material = new Material(image.material);
+            image.material = _material;
+        }
+
+        /// <summary>
+        /// Updates the fill radii based on the normalized team volumes.
+        /// </summary>
+        public void UpdateVolumes(Vector4 teamVolumes)
+        {
+            _colorRadii = new Vector4(
+                teamVolumes.x / upperBound,
+                teamVolumes.y / upperBound,
+                teamVolumes.z / upperBound,
+                teamVolumes.w / upperBound);
+
+            ApplyToMaterial();
+        }
+
+        private void ApplyToMaterial()
+        {
+            if (!_material) return;
+
+            _material.SetFloat("_Radius1", _colorRadii.x);
+            _material.SetFloat("_Radius2", _colorRadii.y);
+            _material.SetFloat("_Radius3", _colorRadii.z);
+            _material.SetFloat("_Radius4", _colorRadii.w);
+        }
+    }
+}
