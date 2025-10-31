4e534298176485ccb0749d1c156af852996286ee
Shombith03 <shombithofficial@outlook.com>
2025-10-31 17:06:10 +0530

    Refactor Vessel HUD Controller into Trail Pool UI and Silhouette Scripts

--- Assets/_Scripts/VesselHUD/View/VesselHUDView.cs
+++ Assets/_Scripts/VesselHUD/View/VesselHUDView.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace CosmicShore.Game
@@ -11,35 +10,20 @@ namespace CosmicShore.Game
         [Serializable]
         public struct HighlightBinding
         {
-            public InputEvents input;  
-            public Image image;        
+            public InputEvents input;
+            public Image image;
         }
-        
+
         [Header("Button highlights")]
         public List<HighlightBinding> highlights = new();
-        
-        [Header("Silhouette Containers")]
-        public Transform silhouetteContainer;
-        public RectTransform trailDisplayContainer;
-        
-        [Header("Silhouette Parts")]
-        public List<GameObject> silhouetteParts = new();
-        
-        [Header("Jaws (Resource Meter)")]
-        public Image topJaw;
-        public Image bottomJaw;
-        public int jawResourceIndex = -1;
-        
-        [FormerlySerializedAs("prismSpawner")] [FormerlySerializedAs("trailSpawner")] [Header("Trail / Drift Sources")]
-        public VesselPrismController vesselPrismController;
-        public DriftTrailActionExecutor driftTrailAction;
 
-        
-        [Header("Trail Config (UI scaling only)")]
-        public float worldToUIScale = 2f;
-        public float imageScale = 0.02f;
-        public bool swingBlocks;
-        
+        [Header("Delegates")]
+        [SerializeField] private VesselSilhouetteController silhouetteController;
+        [SerializeField] private UITrailController          uiTrailController;
+
+        public VesselSilhouetteController Silhouette => silhouetteController;
+        public UITrailController          TrailUI    => uiTrailController;
+
         public void Show() => gameObject.SetActive(true);
         public void Hide() => gameObject.SetActive(false);
     }
