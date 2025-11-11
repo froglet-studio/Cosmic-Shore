3ef3f736fac036c77be97990fa280fd11c25e7d1
Shombith03 <shombithofficial@outlook.com>
2025-11-11 15:29:08 +0530

    Fix Input Block Bug

---
+++ Assets/_Scripts/VesselHUD/Controller/ScriptableEventInputEventBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace CosmicShore.Game
+{
+
+    [CreateAssetMenu(fileName = "ScriptableEventInputEventBlock",
+        menuName = "ScriptableObjects/Events/Input Event Block")]
+    public sealed class ScriptableEventInputEventBlock : ScriptableObject
+    {
+        public event Action<InputEventBlockPayload> OnRaised;
+        public void Raise(InputEventBlockPayload payload) => OnRaised?.Invoke(payload);
+    }
+}
