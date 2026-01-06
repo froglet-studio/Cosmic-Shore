d50cafabbdca747ede27402d998d21bf3719c757
Shombith03 <shombithofficial@outlook.com>
2026-01-06 16:50:53 +0530

    Fix End Game In Wildlife Blitz Game Mode

--- Assets/_Scripts/Game/Managers/StatsManager.cs
+++ Assets/_Scripts/Game/Managers/StatsManager.cs
@@ -160,28 +160,34 @@ namespace CosmicShore.Core
 
             var attackingPlayerName = prismStats.AttackerName;
             var victimPlayerName = prismStats.OwnName;
-            
-            if (!gameData.TryGetRoundStats(attackingPlayerName, out IRoundStats attackerPlayerStats))
-                return;
-            
-            if (!gameData.TryGetRoundStats(victimPlayerName, out IRoundStats victimPlayerStats))
-                return;
-            
-            attackerPlayerStats.BlocksDestroyed++;
-            attackerPlayerStats.TotalVolumeDestroyed += prismStats.Volume;
+
+            var hasAttacker = gameData.TryGetRoundStats(attackingPlayerName, out IRoundStats attackerPlayerStats);
+            var hasVictim = gameData.TryGetRoundStats(victimPlayerName, out IRoundStats victimPlayerStats);
+
+            if (hasAttacker)
+            {
+                attackerPlayerStats.BlocksDestroyed++;
+                attackerPlayerStats.TotalVolumeDestroyed += prismStats.Volume;
+                
+                var isFriendly =
+                    attackingPlayerName == victimPlayerName ||
+                    (hasVictim && attackerPlayerStats.Domain == victimPlayerStats.Domain);
+
+                if (isFriendly)
+                {
+                    attackerPlayerStats.FriendlyPrismsDestroyed++;
+                    attackerPlayerStats.FriendlyVolumeDestroyed += prismStats.Volume;
+                }
+                else
+                {
+                    attackerPlayerStats.HostilePrismsDestroyed++;
+                    attackerPlayerStats.HostileVolumeDestroyed += prismStats.Volume;
+                }
+            }
+
+            if (!hasVictim) return;
             victimPlayerStats.PrismsRemaining--;
             victimPlayerStats.VolumeRemaining -= prismStats.Volume;
-
-            if (attackingPlayerName == victimPlayerName)
-            {
-                attackerPlayerStats.FriendlyPrismsDestroyed++;
-                attackerPlayerStats.FriendlyVolumeDestroyed += prismStats.Volume;
-            }
-            else
-            {
-                attackerPlayerStats.HostilePrismsDestroyed++;
-                attackerPlayerStats.HostileVolumeDestroyed += prismStats.Volume;
-            }
         }
 
         public void PrismRestored(PrismStats prismStats)
