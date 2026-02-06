7ab5cc82d2e0f1cadbde280adc013dbc47ab7d84
Shombith03 <shombithofficial@outlook.com>
2026-02-06 20:54:35 +0530

    Configure Multiplayer Hex Race UI + Create Player Score Card Code

---
+++ Assets/_Scripts/Game/UI/MultiplayerHexRaceHUD.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using CosmicShore.Soap;
+using UnityEngine;
+
+namespace CosmicShore.Game.UI
+{
+    public class MultiplayerHexRaceHUD : MiniGameHUD
+    {
+        [Header("Multiplayer View")]
+        [SerializeField] private MultiplayerHexRaceHUDView multiplayerView;
+
+        private Dictionary<string, PlayerScoreCard> _playerCards = new Dictionary<string, PlayerScoreCard>();
+
+        private void OnValidate()
+        {
+            if (multiplayerView == null)
+                multiplayerView = GetComponent<MultiplayerHexRaceHUDView>();
+            
+            if (view == null)
+                view = multiplayerView;
+        }
+
+        protected override void OnMiniGameTurnStarted()
+        {
+            base.OnMiniGameTurnStarted();
+            InitializePlayerCards();
+        }
+
+        protected override void OnMiniGameTurnEnd()
+        {
+            base.OnMiniGameTurnEnd();
+            UnsubscribeFromAllStats();
+        }
+
+        private void InitializePlayerCards()
+        {
+            multiplayerView.ClearPlayerList();
+            _playerCards.Clear();
+
+            foreach (var stats in gameData.RoundStatsList)
+            {
+                CreateCardForPlayer(stats);
+            }
+        }
+
+        private void CreateCardForPlayer(IRoundStats stats)
+        {
+            var card = Instantiate(multiplayerView.PlayerScoreCardPrefab, multiplayerView.PlayerScoreContainer);
+            
+            var isLocal = gameData.LocalPlayer != null && stats.Name == gameData.LocalPlayer.Name;
+            var teamColor = multiplayerView.GetColorForDomain(stats.Domain);
+            card.Setup(stats.Name, stats.OmniCrystalsCollected, teamColor, isLocal);
+            _playerCards[stats.Name] = card;
+
+            stats.OnOmniCrystalsCollectedChanged += OnCrystalStatChanged;
+        }
+
+        private void OnCrystalStatChanged(IRoundStats updatedStats)
+        {
+            if (_playerCards.TryGetValue(updatedStats.Name, out var card))
+            {
+                card.UpdateScore(updatedStats.OmniCrystalsCollected);
+            }
+        }
+
+        private void UnsubscribeFromAllStats()
+        {
+            foreach (var stats in gameData.RoundStatsList)
+            {
+                stats.OnOmniCrystalsCollectedChanged -= OnCrystalStatChanged;
+            }
+            _playerCards.Clear();
+        }
+    }
+}
---
+++ Assets/_Scripts/Game/UI/MultiplayerHexRaceHUDView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CosmicShore.Models.Enums;
+using UnityEngine;
+
+namespace CosmicShore.Game.UI
+{
+    public class MultiplayerHexRaceHUDView : MiniGameHUDView
+    {
+        [Header("Multiplayer Elements")]
+        [SerializeField] private Transform playerScoreContainer;
+        [SerializeField] private PlayerScoreCard playerScoreCardPrefab;
+
+        [Header("Domain Styling")]
+        [SerializeField] private List<DomainColorDef> domainColors;
+
+        public Transform PlayerScoreContainer => playerScoreContainer;
+        public PlayerScoreCard PlayerScoreCardPrefab => playerScoreCardPrefab;
+
+        public void ClearPlayerList()
+        {
+            foreach (Transform child in playerScoreContainer)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        public Color GetColorForDomain(Domains domain)
+        {
+            var def = domainColors.FirstOrDefault(d => d.Domain == domain);
+            return def.Equals(default(DomainColorDef)) ? Color.white : def.Color;
+        }
+
+        [Serializable]
+        public struct DomainColorDef
+        {
+            public Domains Domain;
+            public Color Color;
+        }
+    }
+}
