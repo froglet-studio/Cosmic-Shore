28e540a9698394a2ab7867161bb22e3d30c1938e
Shombith03 <shombithofficial@outlook.com>
2026-02-11 05:44:26 +0530

    Add Crystal Capture EndGameCinematic, Player Cards, Scoreboard + Add Cinematic Defination + Update UGS Stats Manager

---
+++ Assets/_Scripts/Game/Arcade/MultiplayerCrystalCaptureController.cs
@@ -0,0 +1,14 @@
+﻿namespace CosmicShore.Game.Arcade
+{
+    public class MultiplayerCrystalCaptureController : MultiplayerDomainGamesController
+    {
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            this.numberOfRounds = 1;
+            this.numberOfTurnsPerRound = 1;
+        }
+
+        protected override bool UseGolfRules => false;
+    }
+}
---
+++ Assets/_Scripts/Utility/DataContainers/MultiplayerCrystalCaptureEndGameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Linq;
+using CosmicShore.Game.Cinematics;
+
+namespace CosmicShore.Game.Arcade
+{
+    public class MultiplayerCrystalCaptureEndGameController : EndGameCinematicController
+    {
+        protected override IEnumerator PlayScoreRevealSequence(CinematicDefinitionSO cinematic)
+        {
+            if (!view || !cinematic) yield break;
+
+            view.ShowScoreRevealPanel();
+            view.HideContinueButton();
+    
+            var localPlayerName = gameData.LocalPlayer?.Vessel?.VesselStatus?.PlayerName;
+            var localStats = gameData.RoundStatsList.FirstOrDefault(s => s.Name == localPlayerName);
+
+            if (localStats == null) yield break;
+
+            bool isWinner = gameData.IsLocalDomainWinner(out _);
+            string headerText = isWinner ? "VICTORY" : "DEFEAT";
+            int crystalsCollected = (int)localStats.Score;
+
+            // [Visual Note] Screen overlay: Header text translates upward while the crystal count integer increments rapidly from 0 to target value.
+            yield return view.PlayScoreRevealAnimation(
+                headerText + "\n<size=60%>CRYSTALS COLLECTED</size>",
+                crystalsCollected,
+                cinematic.scoreRevealSettings,
+                false
+            );
+        }
+    }
+}
