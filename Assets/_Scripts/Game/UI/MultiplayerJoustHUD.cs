724fe66403ff0833b0a47cc569b6b59d82b6ffa7
Shombith03 <shombithofficial@outlook.com>
2026-02-12 01:00:05 +0530

    Move Reset Cards to Multiplayer HUD Base Class

--- Assets/_Scripts/Game/UI/MultiplayerJoustHUD.cs
+++ Assets/_Scripts/Game/UI/MultiplayerJoustHUD.cs
@@ -5,42 +5,6 @@ namespace CosmicShore.Game.UI
 {
     public class MultiplayerJoustHUD : MultiplayerHUD
     {
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-
-            if (gameData != null)
-            {
-                gameData.OnMiniGameTurnStarted.OnRaised += RefreshAllPlayerCards;
-                gameData.OnResetForReplay.OnRaised += OnReplayReset;
-            }
-        }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-
-            if (gameData != null)
-            {
-                gameData.OnMiniGameTurnStarted.OnRaised -= RefreshAllPlayerCards;
-                gameData.OnResetForReplay.OnRaised -= OnReplayReset;
-            }
-        }
-
-        /// <summary>
-        /// Called when replay reset happens - reset all collision counts to 0
-        /// </summary>
-        void OnReplayReset()
-        {
-            if (gameData?.RoundStatsList == null) return;
-
-            // Reset all player cards to 0 collisions
-            foreach (var stats in gameData.RoundStatsList.Where(stats => stats != null))
-            {
-                UpdatePlayerCard(stats.Name, 0);
-            }
-        }
-
         protected override int GetInitialCardValue(IRoundStats stats)
         {
             return stats.JoustCollisions;
@@ -67,19 +31,6 @@ namespace CosmicShore.Game.UI
             if (updatedStats != null)
             {
                 UpdatePlayerCard(updatedStats.Name, updatedStats.JoustCollisions);
-            }
-        }
-
-        /// <summary>
-        /// Refresh all player cards - useful when game starts or resets
-        /// </summary>
-        void RefreshAllPlayerCards()
-        {
-            if (gameData?.RoundStatsList == null) return;
-
-            foreach (var stats in gameData.RoundStatsList.Where(stats => stats != null))
-            {
-                UpdatePlayerCard(stats.Name, stats.JoustCollisions);
             }
         }
     }
