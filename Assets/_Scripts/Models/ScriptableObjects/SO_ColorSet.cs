b087e1cc444804b361da307e13802e4131ef5bbb
Yash Sadhukhan <yskhan61@gmail.com>
2026-02-18 03:55:49 +0530

    Update SO_ColorSet.cs

--- Assets/_Scripts/Models/ScriptableObjects/SO_ColorSet.cs
+++ Assets/_Scripts/Models/ScriptableObjects/SO_ColorSet.cs
@@ -23,8 +23,7 @@ public class SO_ColorSet : ScriptableObject
 
         if (colorSet != null) 
             return true;
-        
-        Debug.LogError($"Unknown domain: {domain}, No color set found!");
+
         return false;
     }
 }
