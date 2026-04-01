ca1861bb473e856a49b5a3e475f3714cab49feed
Philip Appoh <appohphil@gmail.com>
2026-04-01 10:41:44 -0500

    Commit 4/1/26

---
+++ Assets/_Scripts/UI/View/FalconHUDVessel.cs
@@ -0,0 +1,15 @@
+using CosmicShore.Game;
+using System.Linq;
+using UnityEngine;
+
+namespace CosmicShore
+{
+    public class FalconHudView : VesselHUDView
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public override void Initialize()
+        {
+          
+        }
+    }
+}
