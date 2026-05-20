d212749cb73ebf3123ada4a7eb8d9d9cb191314a
Philip Appoh <appohphil@gmail.com>
2026-05-20 11:11:07 -0500

    Itrated on BrittleStar

--- Assets/_Scripts/Controller/Vessel/R_VesselActions/Executors/BrittleStarBoostActionExecutor.cs
+++ Assets/_Scripts/Controller/Vessel/R_VesselActions/Executors/BrittleStarBoostActionExecutor.cs
@@ -1,80 +1,67 @@
-using CosmicShore.Game;
-using CosmicShore.Gameplay;
 using Obvious.Soap;
-using System.Diagnostics;
-using System.Threading;
 using UnityEngine;
-namespace CosmicShore
+
+namespace CosmicShore.Gameplay
 {
     public class BrittleStarBoostActionExecutor : ShipActionExecutorBase
     {
-        private IVesselStatus _status; public ScriptableEventNoParam OnMiniGameTurnEnd; private CancellationTokenSource _cts;
-        [SerializeField] private FullAutoActionExecutor FullAutoActionExecutor;
+        [SerializeField] public ScriptableEventNoParam OnMiniGameTurnEnd;
+        [SerializeField] private FullAutoActionExecutor fullAutoActionExecutor;
         [SerializeField] private Transform[] muzzlesMain;
         [SerializeField] private Transform[] muzzlesSecondary;
 
+        private IVesselStatus _status;
+        private bool _boosting;
 
         void OnEnable()
         {
-            if (_status == null) return;
-            _status.IsBoosting = true;
-            _status.VesselTransformer?.ModifyVelocity(_status.Course * 100.0f, 1000);
-            _status.IsStationary = false;
-            ChooseGuns();
-
-            OnMiniGameTurnEnd.OnRaised += OnTurnEndOfMiniGame;
+            if (OnMiniGameTurnEnd != null)
+                OnMiniGameTurnEnd.OnRaised += OnTurnEndOfMiniGame;
         }
 
         void OnDisable()
         {
-            if (_status == null) return;
-            _status.IsBoosting = false;
-            OnMiniGameTurnEnd.OnRaised -= OnTurnEndOfMiniGame;
+            End();
+            if (OnMiniGameTurnEnd != null)
+                OnMiniGameTurnEnd.OnRaised -= OnTurnEndOfMiniGame;
         }
 
         public override void Initialize(IVesselStatus shipStatus)
         {
-
-            base.Initialize(_status);
-
-
+            _status = shipStatus;
         }
-
 
         public void Begin(BrittleStarBoostSO so)
         {
-            //Debug.LogError("Boost Started");
+            if (_status == null) return;
+
+            _boosting = true;
+            _status.IsBoosting = true;
+            _status.VesselTransformer?.ModifyVelocity(_status.Course * 100.0f, 1000);
+            _status.IsStationary = false;
+            ChooseGuns();
         }
 
         public void End()
         {
-            if (_cts == null) return;
+            if (!_boosting) return;
 
+            _boosting = false;
+            if (_status != null)
+                _status.IsBoosting = false;
             ChooseGuns();
-            _cts.Cancel();
-            _cts.Dispose();
-            _cts = null;
-
         }
 
-        void OnTurnEndOfMiniGame()
-        {
-            End();
-        }
+        void OnTurnEndOfMiniGame() => End();
 
         void ChooseGuns()
         {
+            if (fullAutoActionExecutor == null) return;
 
-            if (_status.IsBoosting)
-            {
-                FullAutoActionExecutor.SetGuns(muzzlesSecondary);
-            }
+            if (_boosting)
+                fullAutoActionExecutor.SetGuns(muzzlesSecondary);
             else
-            {
-                FullAutoActionExecutor.SetGuns(muzzlesMain);
-            }
-
+                fullAutoActionExecutor.SetGuns(muzzlesMain);
         }
     }
-
 }
